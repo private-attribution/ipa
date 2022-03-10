0751f24d96e3637c6e16c94aae5997a3cf15555f
Richa Jain <richaj@fb.com>
2022-03-10 11:41:05 +0800

    Privacy Budget Implementation Input : a vector of blinded match keys alongwith budget to be consumed. Output: vector of eligible blinded match keys after consuming their privacy budget(only budget for eligible blinded match keys is consumed)

--- src/error.rs
+++ src/error.rs
@@ -6,6 +6,7 @@ pub enum Error {
     InvalidRole,
     NotEnoughHelpers,
     NotFound,
+    RedisError(redis::RedisError),
     TooManyHelpers,
 
     #[cfg(feature = "cli")]
@@ -49,6 +50,7 @@ forward_errors! {
     std::io::Error => Io,
     #[cfg(feature = "enable-serde")]
     serde_json::Error => Serde,
+    redis::RedisError => RedisError,
 }
 
 pub type Res<T> = Result<T, Error>;
