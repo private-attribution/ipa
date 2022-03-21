4f7bec8ac450f126bddc7cc917af0ae19a094fcc
Thurston Sandberg <thurstonsand@fb.com>
2022-03-21 16:10:07 -0400

    move functions into struct methods

--- benches/esrc.rs
+++ benches/esrc.rs
@@ -14,23 +14,22 @@ use test::Bencher;
 // aka about 20ms
 fn bench_enc_randomize_verify(b: &mut Bencher) {
     let mut rng = StdRng::from_entropy();
-    let (enc_key, _dec_key) = esrc::setup_encryption_keys(&mut rng);
-    let (sign_key, verify_key) = esrc::setup_signing_keys(&mut rng);
+    let e = esrc::Esrc::new(&mut rng);
 
     b.iter(|| {
         // message to send
         let m = G1Affine::from(G1Affine::generator() * Scalar::from(rng.next_u64()));
 
         // initial encryption and signing
-        let enc_m = esrc::encrypt(&mut rng, m, &enc_key);
-        let signature = esrc::sign(&mut rng, &enc_m, &enc_key, sign_key);
+        let cm = e.encrypt(&mut rng, m);
+        let signature = e.sign(&mut rng, &cm);
 
         // rerandomize
         let r_prime = Scalar::from(rng.next_u64());
-        let r_enc_m = esrc::randomize(&enc_m, &enc_key, r_prime);
-        let r_signature = esrc::adapt(&mut rng, &signature, r_prime);
+        let r_cm = e.randomize(&cm, r_prime);
+        let r_signature = e.adapt(&mut rng, &signature, r_prime);
 
         // verify
-        assert!(esrc::verify(&verify_key, &enc_key, &r_enc_m, &r_signature));
+        assert!(e.verify(&r_cm, &r_signature));
     });
 }
