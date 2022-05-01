af4cb2b99dd444fcb79da29da61146e9729fa64b
linanw <linanwang@msn.com>
2022-05-01 12:53:46 +0800

    Secure Coin Tossing
    Created structure, methods and unit test for Secure Coin Tossing

--- src/ss.rs
+++ src/ss.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use sha3::{Digest, Sha3_256};
 
 pub struct BitXORShare {
     share: u32,
@@ -20,9 +21,44 @@ impl BitXORShare {
     }
 }
 
+pub struct SecureCoinTossing {}
+
+impl SecureCoinTossing {
+    pub fn get_random_and_hash() -> (u128, String) {
+        // generate a u128 random integer.
+        let mut rng = rand::thread_rng();
+        let r = rng.gen::<u128>();
+
+        // generate hash for the random integer's bit string.
+        let mut hasher = Sha3_256::new();
+        hasher.update(format!("{:b}", r));
+        let h: String = format!("{:X}", hasher.finalize());
+        (r, h)
+    }
+
+    pub fn get_random() -> u128 {
+        // generate a u128 random integer.
+        let mut rng = rand::thread_rng();
+        rng.gen::<u128>()
+    }
+
+    pub fn verify_random_and_hash(r: u128, h: String) -> bool {
+        // generate hash for the random integer's bit string.
+        let mut hasher = Sha3_256::new();
+        hasher.update(format!("{:b}", r));
+        let hv: String = format!("{:X}", hasher.finalize());
+        h == hv
+    }
+
+    pub fn calculate_c(r1: u128, r2: u128) -> u128 {
+        r1 ^ r2
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::BitXORShare;
+    use super::SecureCoinTossing;
     use rand::Rng;
 
     fn recover_from_shares(share1: u32, share2: u32, share3: u32) -> u32 {
@@ -52,5 +88,27 @@ mod tests {
         );
 
         assert_eq!(integer_matchkey, recovered_integer_matchkey);
+    }
+
+    #[allow(clippy::similar_names)] // deal with it.
+    #[test]
+    fn test_generate_random_u128_and_hash() {
+        let (r1, h1) = SecureCoinTossing::get_random_and_hash();
+        println!("r1: {:b}", r1);
+        println!("h1: {}", h1);
+
+        let r2 = SecureCoinTossing::get_random();
+        println!("r2: {:b}", r2);
+
+        assert_eq!(
+            SecureCoinTossing::verify_random_and_hash(r1, h1.clone()),
+            true
+        );
+        assert_eq!(SecureCoinTossing::verify_random_and_hash(r2, h1), false);
+
+        let c = SecureCoinTossing::calculate_c(r1, r2);
+        println!("c: {:b}", c);
+
+        assert_eq!(c, r1 ^ r2);
     }
 }
