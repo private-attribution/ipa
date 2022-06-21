9ebd688f3b450e868c62d8bf8bace56944bf8ae9
Alex Koshelev <koshelev@fb.com>
2022-06-21 12:09:28 -0700

    Add MPC helper CLI
    CLI starts a http or https server that currently only handles `/echo` requests. For TLS it uses self-signed certificate

--- src/bin/helper.rs
+++ src/bin/helper.rs
@@ -1,67 +1,51 @@
-use log::{error, info};
-use raw_ipa::cli::{HelperArgs, Verbosity};
-use raw_ipa::helpers::{AggregationHelper, EventHelper, Role as HelperRole};
-use std::fs;
+use std::error::Error;
+
+use hyper::http::uri::Scheme;
+use log::info;
+use raw_ipa::cli::Verbosity;
+use raw_ipa::net::{bind_mpc_helper_server, BindTarget};
+use std::net::SocketAddr;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-struct CommonArgs {
-    #[structopt(flatten)]
+#[structopt(name = "mpc-helper", about = "CLI to start an MPC helper endpoint")]
+struct Args {
     /// Configure logging.
+    #[structopt(flatten)]
     logging: Verbosity,
 
-    #[structopt(flatten)]
-    helpers: HelperArgs,
+    /// Port to listen. If not specified, will ask Kernel to assign the port
+    #[structopt(short = "p", long = "port")]
+    port: Option<u16>,
+
+    /// Indicates whether to start HTTP or HTTPS endpoint
+    #[structopt(short = "-s", long = "scheme", default_value = "http")]
+    scheme: Scheme,
 }
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "helper", about = "Functions for IPA helper servers")]
-struct Args {
-    #[structopt(flatten)]
-    common: CommonArgs,
-
-    #[structopt(subcommand)]
-    action: Action,
-}
-
-#[derive(Debug, StructOpt)]
-#[structopt(name = "action")]
-enum Action {
-    /// Setup a helper.
-    Setup {
-        /// The type of helper to configure.
-        helper: HelperRole,
-    },
-}
-
-impl Action {
-    fn dispatch(&self, common: &CommonArgs) {
-        match self {
-            Self::Setup { helper } => {
-                info!("Setup helper {}", helper);
-                let dir = &common.helpers[*helper];
-                if dir.exists() {
-                    error!("Helper directory exists");
-                    return;
-                }
-                fs::create_dir_all(dir).unwrap();
-                match helper {
-                    HelperRole::Event(r) => {
-                        let h = EventHelper::new(*r);
-                        h.save(dir).unwrap();
-                    }
-                    HelperRole::Aggregation(r) => {
-                        let h = AggregationHelper::new(*r);
-                        h.save(dir).unwrap();
-                    }
-                }
-            }
-        }
-    }
-}
-
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::from_args();
-    args.common.logging.setup_logging();
-    args.action.dispatch(&args.common);
+    args.logging.setup_logging();
+
+    // decide what protocol we're going to use here
+    let addr = SocketAddr::from(([127, 0, 0, 1], args.port.unwrap_or(0)));
+    let target = match args.scheme.as_str() {
+        "http" => BindTarget::Http(addr),
+        #[cfg(feature = "self-signed-certs")]
+        "https" => {
+            let config = raw_ipa::net::tls_config_from_self_signed_cert().await?;
+            BindTarget::Https(addr, config)
+        }
+        _ => {
+            panic!("\"{}\" protocol is not supported", args.scheme)
+        }
+    };
+
+    // start server
+    let (addr, server_handle) = bind_mpc_helper_server(target).await;
+    info!("listening to {}://{}", args.scheme, addr);
+    server_handle.await?;
+
+    Ok(())
 }
