f1f6296550cd0a0a112e1e2a0f796c22efdf1eb7
Taiki Yamaguchi <taiki.yamaguchi@gmail.com>
2022-07-04 18:33:05 +0800

    Add MPC test client CLI

---
+++ src/bin/test_mpc.rs
@@ -0,0 +1,35 @@
+use raw_ipa::cli::Verbosity;
+use raw_ipa::net::{Client, Command, MpcHandle};
+use std::error::Error;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "mpc-client",
+    about = "CLI to execute test scenarios on IPA MPC helpers"
+)]
+struct Args {
+    #[structopt(flatten)]
+    logging: Verbosity,
+
+    #[structopt(short, long)]
+    uri: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::from_args();
+    args.logging.setup_logging();
+
+    // TODO: Start MPC helpers and discover
+    let client = Client::new(args.uri.as_str());
+
+    let response = client.execute(Command::Echo("hello".into())).await?;
+
+    println!(
+        "Response: {:?}",
+        String::from_utf8_lossy(response.as_slice())
+    );
+
+    Ok(())
+}
