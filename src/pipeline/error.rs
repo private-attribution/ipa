b3f22ee7223169cb628b5aa84cf95af3eeabbb7d
Thurston Sandberg <thurstonsand@fb.com>
2022-07-05 19:35:05 -0400

    one possible interface for a pipeline

--- src/pipeline/error.rs
+++
@@ -1,7 +0,0 @@
-use std::fmt::Debug;
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum Error {}
-
-pub type Result<T> = std::result::Result<T, Error>;
--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -1,81 +1,49 @@
-pub mod error;
+use futures::FutureExt;
+use futures::Stream;
+use futures::StreamExt;
 
-pub use error::Result;
+pub struct MultiplySumAndStringify {}
 
-use async_trait::async_trait;
-use futures::stream::Stream;
-use futures::{SinkExt, StreamExt};
-
-#[async_trait]
-pub trait Step {
-    type Input: Send + Sync;
-    type Output: Send + Sync;
-    async fn compute(&self, inp: Self::Input) -> Result<Self::Output>;
-}
-
-pub struct StreamStep<
-    Input: Send + Sync,
-    Output: Send + Sync,
-    S: Step<Input = Input, Output = Output> + Send + Sync,
->(S);
-// TODO: any way to have this impl `Step`?
-impl<
-        Input: Send + Sync,
-        Output: Send + Sync + 'static,
-        S: Step<Input = Input, Output = Output> + Send + Sync + 'static,
-    > StreamStep<Input, Output, S>
-{
-    pub async fn compute(
-        self,
-        inps: impl Stream<Item = Input> + Send + 'static,
-    ) -> impl Stream<Item = Result<Output>> {
-        let (mut tx, rx) = futures::channel::mpsc::channel(32);
-        tokio::spawn(async move {
-            let mut pinned = Box::pin(inps);
-            while let Some(inp) = pinned.next().await {
-                let _ = tx.send(self.0.compute(inp).await).await;
-            }
-        });
-        rx
-        // TODO: below does not compile
-        // inps.then(|inp| self.0.compute(inp))
+impl MultiplySumAndStringify {
+    pub async fn run(&self, inp: impl Stream<Item = i32>) -> String {
+        // chunks provides a way to buffer results as needed.
+        // for instance, if deserializing a certain number of bytes, you could chunk them
+        inp.chunks(2)
+            // map provides a way to act on the collected chunk.
+            // in this case, multiply the 2 chunked values
+            .map(|v| {
+                if v.len() != 2 {
+                    panic!("invalid number of integers in stream; must be even number")
+                }
+                v[0] * v[1]
+            })
+            // fold provides a way to accumulate all values at the end of computation.
+            // in this case, sum them
+            .fold(0, |acc, i| async move { acc + i })
+            // finally, it is possible to further work on the value, since it is within a future.
+            // alternatively, just `await` the future and act on the value at that time.
+            // in this case, stringify
+            .map(|i| i.to_string())
+            .await
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use async_trait::async_trait;
+    use futures::stream;
 
     #[tokio::test]
-    async fn async_example() {
-        struct AsyncStep {}
-
-        #[async_trait]
-        impl Step for AsyncStep {
-            type Input = (i32, i32);
-            type Output = i32;
-
-            async fn compute(&self, inp: Self::Input) -> Result<Self::Output> {
-                Ok(inp.0 + inp.1)
-            }
-        }
-
-        let iter = vec![(1, 1), (2, 2), (3, 3), (4, 4)];
-        let expected_iter = iter
-            .clone()
-            .into_iter()
-            .map(|(x, y)| x + y)
-            .collect::<Vec<_>>();
-
-        let iter_stream = futures::stream::iter(iter);
-        let step = AsyncStep {};
-        let stream_step = StreamStep(step);
-
-        let res_stream = stream_step.compute(iter_stream).await;
-        let res = res_stream.collect::<Vec<_>>().await;
-        let res = res.into_iter().collect::<Result<Vec<_>>>().unwrap();
-
-        assert_eq!(expected_iter, res);
+    async fn usage() {
+        let pipe = MultiplySumAndStringify {};
+        let inp = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let expected_res = inp
+            .chunks(2)
+            .map(|v| v[0] * v[1])
+            .fold(0, |acc, i| acc + i)
+            .to_string();
+        let inp_stream = stream::iter(inp);
+        let res = pipe.run(inp_stream).await;
+        assert_eq!(expected_res, res)
     }
 }
