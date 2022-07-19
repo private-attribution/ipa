75ab9411f6f3dece6b05b03d4ae367adf447fb43
Ben Savage <btsavage@fb.com>
2022-07-20 01:47:33 +0800

    Adding a ReplicatedSecretSharing implementation with support for addition, subtraction and multiplication by a constant

---
+++ src/replicated_secret_sharing.rs
@@ -0,0 +1,191 @@
+use std::{
+    fmt::Debug,
+    ops::{Add, Mul, Neg, Sub},
+};
+
+use crate::field::Field;
+use crate::prss::Participant;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ReplicatedSecretSharing<T>(T, T);
+
+impl<T: Field> ReplicatedSecretSharing<T> {
+    #[must_use]
+    pub fn construct(a: T, b: T) -> Self {
+        Self(a, b)
+    }
+}
+
+impl<T: Field> Add for ReplicatedSecretSharing<T> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl<T: Field> Neg for ReplicatedSecretSharing<T> {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self(-self.0, -self.1)
+    }
+}
+
+impl<T: Field> Sub for ReplicatedSecretSharing<T> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl<T: Field> Mul<T> for ReplicatedSecretSharing<T> {
+    type Output = Self;
+
+    fn mul(self, rhs: T) -> Self {
+        Self(rhs * self.0, rhs * self.1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::replicated_secret_sharing::ReplicatedSecretSharing;
+
+    use crate::field::Fp31;
+
+    fn secret_share(
+        a: u8,
+        b: u8,
+        c: u8,
+    ) -> (
+        ReplicatedSecretSharing<Fp31>,
+        ReplicatedSecretSharing<Fp31>,
+        ReplicatedSecretSharing<Fp31>,
+    ) {
+        (
+            ReplicatedSecretSharing::construct(Fp31::from(a), Fp31::from(b)),
+            ReplicatedSecretSharing::construct(Fp31::from(b), Fp31::from(c)),
+            ReplicatedSecretSharing::construct(Fp31::from(c), Fp31::from(a)),
+        )
+    }
+
+    fn assert_valid_secret_sharing(
+        res1: ReplicatedSecretSharing<Fp31>,
+        res2: ReplicatedSecretSharing<Fp31>,
+        res3: ReplicatedSecretSharing<Fp31>,
+    ) {
+        assert_eq!(res1.1, res2.0);
+        assert_eq!(res2.1, res3.0);
+        assert_eq!(res3.1, res1.0);
+    }
+
+    fn assert_secret_shared_value(
+        a1: ReplicatedSecretSharing<Fp31>,
+        a2: ReplicatedSecretSharing<Fp31>,
+        a3: ReplicatedSecretSharing<Fp31>,
+        expected_value: u128,
+    ) {
+        assert_eq!(a1.0 + a2.0 + a3.0, Fp31::from(expected_value));
+        assert_eq!(a1.1 + a2.1 + a3.1, Fp31::from(expected_value));
+    }
+
+    fn addition_test_case(a: (u8, u8, u8), b: (u8, u8, u8), expected_output: u128) {
+        let (a1, a2, a3) = secret_share(a.0, a.1, a.2);
+        let (b1, b2, b3) = secret_share(b.0, b.1, b.2);
+
+        // Compute r1 + r2
+        let res1 = a1 + b1;
+        let res2 = a2 + b2;
+        let res3 = a3 + b3;
+
+        assert_valid_secret_sharing(res1, res2, res3);
+        assert_secret_shared_value(res1, res2, res3, expected_output);
+    }
+
+    #[test]
+    fn test_simple_addition() {
+        addition_test_case((1, 0, 0), (1, 0, 0), 2);
+        addition_test_case((1, 0, 0), (0, 1, 0), 2);
+        addition_test_case((1, 0, 0), (0, 0, 1), 2);
+
+        addition_test_case((0, 1, 0), (1, 0, 0), 2);
+        addition_test_case((0, 1, 0), (0, 1, 0), 2);
+        addition_test_case((0, 1, 0), (0, 0, 1), 2);
+
+        addition_test_case((0, 0, 1), (1, 0, 0), 2);
+        addition_test_case((0, 0, 1), (0, 1, 0), 2);
+        addition_test_case((0, 0, 1), (0, 0, 1), 2);
+
+        addition_test_case((0, 0, 0), (1, 0, 0), 1);
+        addition_test_case((0, 0, 0), (0, 1, 0), 1);
+        addition_test_case((0, 0, 0), (0, 0, 1), 1);
+
+        addition_test_case((1, 0, 0), (0, 0, 0), 1);
+        addition_test_case((0, 1, 0), (0, 0, 0), 1);
+        addition_test_case((0, 0, 1), (0, 0, 0), 1);
+
+        addition_test_case((0, 0, 0), (0, 0, 0), 0);
+
+        addition_test_case((1, 3, 5), (10, 0, 2), 21);
+    }
+
+    fn subtraction_test_case(a: (u8, u8, u8), b: (u8, u8, u8), expected_output: u128) {
+        let (a1, a2, a3) = secret_share(a.0, a.1, a.2);
+        let (b1, b2, b3) = secret_share(b.0, b.1, b.2);
+
+        // Compute r1 - r2
+        let res1 = a1 - b1;
+        let res2 = a2 - b2;
+        let res3 = a3 - b3;
+
+        assert_valid_secret_sharing(res1, res2, res3);
+        assert_secret_shared_value(res1, res2, res3, expected_output);
+    }
+
+    #[test]
+    fn test_simple_subtraction() {
+        subtraction_test_case((1, 0, 0), (1, 0, 0), 0);
+        subtraction_test_case((1, 0, 0), (0, 1, 0), 0);
+        subtraction_test_case((1, 0, 0), (0, 0, 1), 0);
+
+        subtraction_test_case((0, 1, 0), (1, 0, 0), 0);
+        subtraction_test_case((0, 1, 0), (0, 1, 0), 0);
+        subtraction_test_case((0, 1, 0), (0, 0, 1), 0);
+
+        subtraction_test_case((0, 0, 1), (1, 0, 0), 0);
+        subtraction_test_case((0, 0, 1), (0, 1, 0), 0);
+        subtraction_test_case((0, 0, 1), (0, 0, 1), 0);
+
+        subtraction_test_case((0, 0, 0), (1, 0, 0), 30);
+        subtraction_test_case((0, 0, 0), (0, 1, 0), 30);
+        subtraction_test_case((0, 0, 0), (0, 0, 1), 30);
+
+        subtraction_test_case((1, 0, 0), (0, 0, 0), 1);
+        subtraction_test_case((0, 1, 0), (0, 0, 0), 1);
+        subtraction_test_case((0, 0, 1), (0, 0, 0), 1);
+
+        subtraction_test_case((0, 0, 0), (0, 0, 0), 0);
+
+        subtraction_test_case((1, 3, 5), (10, 0, 2), 28);
+    }
+
+    fn mult_by_constant_test_case(a: (u8, u8, u8), c: u8, expected_output: u128) {
+        let (a1, a2, a3) = secret_share(a.0, a.1, a.2);
+
+        let res1 = a1 * Fp31::from(c);
+        let res2 = a2 * Fp31::from(c);
+        let res3 = a3 * Fp31::from(c);
+
+        assert_valid_secret_sharing(res1, res2, res3);
+        assert_secret_shared_value(res1, res2, res3, expected_output);
+    }
+
+    #[test]
+    fn test_mult_by_constant() {
+        mult_by_constant_test_case((1, 0, 0), 2, 2);
+        mult_by_constant_test_case((0, 1, 0), 2, 2);
+        mult_by_constant_test_case((0, 0, 1), 2, 2);
+        mult_by_constant_test_case((0, 0, 0), 2, 0);
+    }
+}
