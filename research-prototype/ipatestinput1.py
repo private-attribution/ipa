b5387ca638f0e671f360b7bfebc8e10d7e656a42
Benjamin M. Case <35273659+bmcase@users.noreply.github.com>
2022-08-05 16:50:49 -0400

    adding some basic test cases
    basic test cases with the expected output for use in debugging.

---
+++ research-prototype/ipatestinput1.py
@@ -0,0 +1,43 @@
+import argparse
+import random
+from pathlib import Path
+
+parser = argparse.ArgumentParser(
+    description="Generate random IPA input data for MP-SPDZ"
+)
+parser.add_argument("numrows_power", type=int, help="Generate 2^n inputs")
+
+args = parser.parse_args()
+
+numrows = 2**args.numrows_power
+approx_rows_per_mk = 10
+
+modulus = 2**64
+mkmod = 2**31
+datamod = 2**8
+breakdown_keys = 4
+
+player_data = Path("Player-Data")
+player_data.mkdir(parents=True, exist_ok=True)
+
+with open(player_data / "Input-P0-0", "w") as f:
+    separator = " "
+
+    f.write(f"3" + separator + "0" + separator + "0" + separator + "1\n")
+    f.write(f"3" + separator + "1" + separator + "11" + separator + "0\n")
+    f.write(f"2" + separator + "1" + separator + "7" + separator + "0\n")
+    f.write(f"2" + separator + "0" + separator + "0" + separator + "2\n")
+    f.write(f"4" + separator + "0" + separator + "0" + separator + "3\n")
+    f.write(f"4" + separator + "1" + separator + "8" + separator + "0\n")
+    f.write(f"4" + separator + "1" + separator + "6" + separator + "0\n")
+    
+    for i in range(7,numrows):
+    	f.write(f"0"+separator +"0"+separator+"0"+separator +"0\n" )
+
+
+print(f"wrote {numrows} rows")
+print(f"output should be breakdown 1: 11, breakdown 2: 0, breakdown 3: 14")
+
+
+
+
