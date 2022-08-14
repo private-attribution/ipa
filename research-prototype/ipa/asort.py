b5e673bdf992ce2dbf43683009ba50c3e004f2ae
Erik Taubeneck <erik.taubeneck@gmail.com>
2022-08-13 18:13:49 -0700

    add cli and merge vectorized into ipae2e

--- research-prototype/ipa/asort.py
+++ research-prototype/ipa/asort.py
@@ -1,10 +1,13 @@
-import itertools
-from Compiler import types, library, instructions
+from Compiler.instructions import delshuffle
+from Compiler.library import for_range, break_point, if_, else_, if_e
+from Compiler.types import Array, Matrix, sint, regint
+from Compiler.sorting import radix_sort as mp_spdz_radix_sort
+
 
 def dest_comp(B):
     """
     Compute the 'destination' permutation
-    
+
     Calculate the permutation to stable sort a bit vector.
     In the original, we multiply have of the cumulative sums
     by the bits, and half by the complements of the bits.
@@ -19,15 +22,17 @@ def dest_comp(B):
     St_flat = Bt.value_type.Array(len(Bt_flat))
     St_flat.assign(Bt_flat)
     num = len(St_flat) // 2
-    @library.for_range(len(St_flat) - 1)
+
+    @for_range(len(St_flat) - 1)
     def _(i):
         St_flat[i + 1] = St_flat[i + 1] + St_flat[i]
-    cumval = St_flat.get_vector(size = num)
-    cumshift = St_flat.get_vector(base = num, size = num) - cumval
-    dest = cumval + Bt_flat.get_vector(base = num, size = num) * cumshift - 1
-    Tt = types.Array(num, B.value_type)
+    cumval = St_flat.get_vector(size=num)
+    cumshift = St_flat.get_vector(base=num, size=num) - cumval
+    dest = cumval + Bt_flat.get_vector(base=num, size=num) * cumshift - 1
+    Tt = Array(num, B.value_type)
     Tt.assign_vector(dest)
     return Tt
+
 
 def reveal_sort(k, D, reverse=False):
     """
@@ -35,97 +40,127 @@ def reveal_sort(k, D, reverse=False):
     Rearrange D by k.
     """
     assert len(k) == len(D)
-    library.break_point()
-    shuffle = types.sint.get_secure_shuffle(len(k))
+    break_point()
+    shuffle = sint.get_secure_shuffle(len(k))
     k_prime = k.get_vector().secure_permute(shuffle).reveal()
-    idx = types.Array.create_from(k_prime)
+    idx = Array.create_from(k_prime)
     if D:
         reverse.assign_vector(D.get_slice_vector(idx))
-        library.break_point()
+        break_point()
         D.secure_permute(shuffle, reverse=True)
     else:
         D.secure_permute(shuffle)
-        library.break_point()
+        break_point()
         v = D.get_vector()
         D.assign_slice_vector(idx, v)
-    library.break_point()
-    instructions.delshuffle(shuffle)
+    break_point()
+    delshuffle(shuffle)
+
 
 def double_dest(bs):
     """
     bs is an n by 2 bit array.
     """
     num, _ = bs.sizes
-    bits = types.sint.Array(num * 4)
+    # BUG this is never used
+    bits = sint.Array(num * 4)
     col0 = bs.get_column(0)
     col1 = bs.get_column(1)
     prod = col0 * col1
-    cum.assign_vector(prod - col0 - col1 + 1) # 00
-    cum.assign_vector(col1 - prod, base = num) # 01
-    cum.assign_vector(col0 - prod, base = 2 * num) # 10
-    cum.assign_vector(prod, base = 3 * num) # 11
-    @library.for_range(len(B) - 1)
+    # BUG this is never assigned
+    cum.assign_vector(prod - col0 - col1 + 1)  # 00
+    cum.assign_vector(col1 - prod, base=num)  # 01
+    cum.assign_vector(col0 - prod, base=2 * num)  # 10
+    cum.assign_vector(prod, base=3 * num)  # 11
+
+    @for_range(len(B) - 1)
     def _(i):
         cum[i + 1] = cum[i + 1] + cum[i]
-    one_contrib = cum.get_vector(size = num)
-    col0_contrib = (cum.get_vector(base = 2 * num, size = num)
+
+    one_contrib = cum.get_vector(size=num)
+    col0_contrib = (cum.get_vector(base=2 * num, size=num)
                     - one_contrib)
-    col1_contrib = (cum.get_vector(base = num, size = num)
+    col1_contrib = (cum.get_vector(base=num, size=num)
                     - one_contrib)
     prod_contrib = (one_contrib
-                    + cum.get_vector(base = 3 * num, size = num))
+                    + cum.get_vector(base=3*num, size=num))
     return (one_contrib
             + col0 * col0_contrib
             + col1 * col1_contrib
-            + prod * prod_contrib -1)
-    
+            + prod * prod_contrib-1)
+
+
 def double_bit_radix_sort(bs, D):
     """
     Use two bits at a time.
     There's an annoying problem if n_bits is odd.
     """
     n_bits, num = bs.sizes
-    h = types.Array.create_from(types.sint(types.regint.inc(num)))
+    h = Array.create_from(sint(regint.inc(num)))
+
     # Test if n_bits is odd
-    @library.for_range(n_bits // 2)
+    @for_range(n_bits // 2)
     def _(i):
         perm = double_dest(bs[2 * i: 2 * i + 2])
-        reveal_sort(perm, h, reverse = False)
-        @library.if_e(2 * i + 3 < n_bits)
-        def _(): # sort the next 2 bits
-            reveal_sort(h, bs[2 * i + 2: 2 * i + 4], reverse = True)
-        @library.else_
+        reveal_sort(perm, h, reverse=False)
+
+        @if_e(2 * i + 3 < n_bits)
+        def _():  # sort the next 2 bits
+            reveal_sort(h, bs[2 * i + 2: 2 * i + 4], reverse=True)
+
+        @else_
         def _():
-            @library.if_(n_bits % 2 == 1)
+            @if_(n_bits % 2 == 1)
             def odd_case():
-                reveal_sort(h, bs[-1], reverse == True)
-                c = types.Array.create_from(dest_comp(bs[-1]))
+                reveal_sort(h, bs[-1], reverse=True)
+                c = Array.create_from(dest_comp(bs[-1]))
                 reveal_sort(c, h, reverse=False)
     # Now take care of the odd case
-    reveal_sort(h, D, reverse == True)
-            
-def bit_radix_sort(bs, D):
+    reveal_sort(h, D, reverse=True)
 
+
+def bit_radix_sort(bs, D):
     n_bits, num = bs.sizes
-    B = types.sint.Matrix(num, 2)
-    h = types.Array.create_from(types.sint(types.regint.inc(num)))
-    @library.for_range(n_bits)
+    B = sint.Matrix(num, 2)
+    h = Array.create_from(sint(regint.inc(num)))
+
+    @for_range(n_bits)
     def _(i):
         b = bs[i]
         B.set_column(0, 1 - b.get_vector())
         B.set_column(1, b.get_vector())
-        c = types.Array.create_from(dest_comp(B))
+        c = Array.create_from(dest_comp(B))
         reveal_sort(c, h, reverse=False)
-        @library.if_e(i < n_bits - 1)
+
+        @if_e(i < n_bits - 1)
         def _():
             reveal_sort(h, bs[i + 1], reverse=True)
-        @library.else_
+
+        @else_
         def _():
             reveal_sort(h, D, reverse=True)
 
+
 def radix_sort(k, D, n_bits=None, signed=True):
     assert len(k) == len(D)
-    bs = types.Matrix.create_from(k.get_vector().bit_decompose(n_bits))
+    bs = Matrix.create_from(k.get_vector().bit_decompose(n_bits))
     if signed and len(bs) > 1:
         bs[-1][:] = bs[-1][:].bit_not()
     bit_radix_sort(bs, D)
+
+
+def two_bit_radix_sort(k, D, n_bits):
+    # BUG: radix sort doesn't accept two_bit...
+    return radix_sort(k, D, n_bits, two_bit=True)
+
+
+def batcher_sort(k, D, n_bits):
+    raise NotImplementedError("Batcher not implemented")
+
+
+sort_functions = {
+    'LIBRARY_SORT': mp_spdz_radix_sort,
+    'RADIX_SORT': radix_sort,
+    'TWO_BIT_RADIX_SORT': two_bit_radix_sort,
+    'BATCHER_SORT': batcher_sort,
+}
---
+++ research-prototype/ipa/cli.py
@@ -0,0 +1,202 @@
+""" Interoperable Private Attribution - Research Prototype Aggregator using MP-SPDZ
+
+Usage:
+  ipa compile [--numrows_power=<n>] [--breakdown_values=<b>] [--n_bits=<n_bits>]
+              [--radix_sort | --two_bit_radix_sort | --batcher_sort]
+              [--sequential_capping] [--verbose_compile_filename]
+              [--skip_sort] [--skip_attribution] [--skip_capping] [--skip_aggregation]
+
+
+Options:
+  --numrows_power=<n>         Run for 2^n input rows. [default: 4]
+  --breakdown_values=<b>      Number of Breakdown keys. [default: 4]
+  --n_bits=<n_bits>           Number of bits used for matchkeys. [default: 32]
+  --radix_sort                Use radix_sort. default: library_sort
+  --two_bit_radix_sort        Use two_bit_radix_sort. default: library_sort
+  --batcher_sort              Use batcher_sorts (NOT IMPLEMENTED.) default: library_sort
+  --sequential_capping        Use sequential_capping. default: paralell
+  --verbose_compile_filename  Compiled filename reflects args.
+  --skip_sort                 Skip the sort (for performance measurement.)
+  --skip_attribution          Skip attribution (for performance measurement.)
+  --skip_capping              Skip capping (for performance measurement.)
+  --skip_aggregation          Skip aggregation (for performance measurement.)
+"""
+from docopt import docopt
+from schema import Schema, Use, Or
+
+from ipae2e import (
+    load_data,
+    oblivious_attribution,
+    sequential_capping,
+    parallel_capping,
+    aggregate,
+)
+from asort import sort_functions
+
+from Compiler.compilerLib import Compiler
+from Compiler.library import print_ln
+from Compiler.types import Array, sint, sintbit
+
+
+def parse_mutually_exclusive_options(args, options, new_arg_name, default=None):
+    """
+    Converts a dict <args> containing the keys in <options> and updates
+    <args[new_arg_name]> to the exclusively true value in <options>.
+    """
+<<<<<<< Updated upstream
+    selected_options = [
+        k for k, v in args.items()
+        if k in options and v
+    ]
+=======
+    selected_options = [k for k, v in args.items() if k in options and v]
+>>>>>>> Stashed changes
+    if not selected_options and default is None:
+        raise Exception(f"One the following must be selected: {options}")
+    elif len(selected_options) > 1:
+        raise Exception(f"Can only select one of {selected_options}")
+    for option in options:
+        if option in args:
+            del args[option]
+    selected_options.append(default)
+    args[new_arg_name] = selected_options[0]
+    return args
+
+
+def clean_args(args):
+    """
+    Remove "--" and upper case all keys in args.
+    """
+    return {k.strip("--").upper(): v for k, v in args.items()}
+
+
+def validate_args(args):
+    args = clean_args(args)
+    args = parse_mutually_exclusive_options(
+        args,
+        sort_functions.keys(),
+        "SORT_FUNCTION_NAME",
+        default="LIBRARY_SORT",
+    )
+
+    capping_types = ["SEQUENTIAL_CAPPING", "PARALLEL_CAPPING"]
+    args = parse_mutually_exclusive_options(
+        args,
+        capping_types,
+        "CAPPING_TYPE",
+        default="PARALLEL_CAPPING",
+    )
+
+<<<<<<< Updated upstream
+    schema = Schema({
+        "SORT_FUNCTION_NAME": Or(*sort_functions.keys()),
+        "CAPPING_TYPE": Or(*capping_types),
+        "NUMROWS_POWER": Use(int),
+        "BREAKDOWN_VALUES": Use(int),
+        "N_BITS": Use(int),
+        "COMPILE": Use(bool),
+        "VERBOSE_COMPILE_FILENAME": Use(bool),
+        "SKIP_SORT": Use(bool),
+        "SKIP_ATTRIBUTION": Use(bool),
+        "SKIP_CAPPING": Use(bool),
+        "SKIP_AGGREGATION": Use(bool),
+    })
+=======
+    schema = Schema(
+        {
+            "SORT_FUNCTION_NAME": Or(*sort_functions.keys()),
+            "CAPPING_TYPE": Or(*capping_types),
+            "NUMROWS_POWER": Use(int),
+            "BREAKDOWN_VALUES": Use(int),
+            "N_BITS": Use(int),
+            "COMPILE": Use(bool),
+            "VERBOSE_COMPILE_FILENAME": Use(bool),
+            "SKIP_SORT": Use(bool),
+            "SKIP_ATTRIBUTION": Use(bool),
+            "SKIP_CAPPING": Use(bool),
+            "SKIP_AGGREGATION": Use(bool),
+        }
+    )
+>>>>>>> Stashed changes
+    args = schema.validate(args)
+    return args
+
+
+def compiled_filename(args):
+    filename = "ipae2e"
+    keys_to_ignore = {"VERBOSE_COMPILE_FILENAME", "COMPILE"}
+    if args["VERBOSE_COMPILE_FILENAME"]:
+        for k, v in sorted(args.items()):
+            if k not in keys_to_ignore:
+                if type(v) == bool:
+                    filename += f"___{k}"
+                else:
+                    filename += f"___{k}__{v}"
+    return filename
+
+
+def _compile(args):
+<<<<<<< Updated upstream
+    numrows = 2**args["NUMROWS_POWER"]
+=======
+    numrows = 2 ** args["NUMROWS_POWER"]
+>>>>>>> Stashed changes
+    sort_function = sort_functions[args["SORT_FUNCTION_NAME"]]
+    capping_functions = {
+        "SEQUENTIAL_CAPPING": sequential_capping,
+        "PARALLEL_CAPPING": parallel_capping,
+    }
+    capping_function = capping_functions[args["CAPPING_TYPE"]]
+    breakdown_values = args["BREAKDOWN_VALUES"]
+    skip_sort = args["SKIP_SORT"]
+    skip_attribution = args["SKIP_ATTRIBUTION"]
+    skip_capping = args["SKIP_CAPPING"]
+    skip_aggregation = args["SKIP_AGGREGATION"]
+
+    compiler = Compiler(custom_args=["compile.py", "-C", "-R", "32"])
+
+    filename = compiled_filename(args)
+    print(f"Compiling {filename}")
+
+    @compiler.register_function(filename)
+    def ipae2e():
+        # load the data
+        reports, match_keys = load_data(numrows)
+
+        if not skip_sort:
+            # BUG: function calls like ths shouldn't have a side effect.
+            # it should ether return back a new reports object, or be a
+            # method on the reports object
+            sort_function(match_keys, reports)
+
+        if not skip_attribution:
+            helperbits, final_credits = oblivious_attribution(
+                reports,
+                breakdown_values,
+            )
+        else:
+            helperbits = Array(numrows, sintbit)
+            helperbits.assign_all(0)
+            final_credits = Array(numrows, sint)
+            helperbits.assign_all(0)
+
+        if not skip_capping:
+            final_credits = capping_function(numrows, final_credits, helperbits)
+
+        if not skip_aggregation:
+            breakdown_key_sums = aggregate(reports, breakdown_values, final_credits)
+            print_ln("breakdowns: %s", breakdown_key_sums.reveal())
+
+    compiler.compile_func()
+
+
+def main():
+    args = docopt(__doc__)
+    args = validate_args(args)
+
+    if args["COMPILE"]:
+        _compile(args)
+
+
+if __name__ == "__main__":
+    main()
