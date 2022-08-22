dd5b5654130ccb81ecdb153085dfc70a08042e0b
Alex Koshelev <koshelev@fb.com>
2022-08-22 13:58:43 -0700

    Move step to `protocol` folder

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 #![deny(clippy::clone_on_ref_ptr)]
 
 mod chunkscan;
-mod circuit;
 #[cfg(feature = "cli")]
 pub mod cli;
 pub mod error;
 pub mod field;
 pub mod helpers;
 pub mod net;
+pub mod protocol;
 pub mod prss;
 mod replicated_secret_sharing;
 pub mod report;
