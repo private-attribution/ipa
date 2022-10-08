bb51f6e1a3cdf5d362bcd8a24faff88394c21522
Alex Koshelev <koshelev@meta.com>
2022-10-07 20:42:59 -0700

    Upgrade to clap
    Clap includes everything that structopt offers now and more. Structopt is in support mode only now, so it may make sense to migrate to clap while IPA is still young.
    
    https://epage.github.io/blog/2022/09/clap4/

--- benches/oneshot/arithmetic_circuit.rs
+++ benches/oneshot/arithmetic_circuit.rs
@@ -1,29 +1,29 @@
+use clap::Parser;
 use raw_ipa::field::{Field, Fp31};
 use raw_ipa::test_fixture::circuit;
 use std::time::Instant;
-use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, Parser)]
 pub struct CircuitArgs {
-    #[structopt(
+    #[arg(
         short,
         long,
         help = "width of the circuit, defines how many operations can proceed in parallel"
     )]
     pub width: u32,
 
-    #[structopt(short, long, help = "depth of the circuit")]
+    #[arg(short, long, help = "depth of the circuit")]
     pub depth: u8,
 
     /// Cargo passes the bench argument
     /// https://doc.rust-lang.org/cargo/commands/cargo-bench.html
-    #[structopt(short, long, help = "ignored")]
+    #[arg(short, long, help = "ignored")]
     pub bench: bool,
 }
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 3)]
 pub async fn main() {
-    let args = CircuitArgs::from_args();
+    let args = CircuitArgs::parse();
 
     {
         let field_size = <Fp31 as Field>::Integer::BITS;
