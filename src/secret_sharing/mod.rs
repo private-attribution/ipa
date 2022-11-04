1695cdedf11605356b3233852f06b606f6eacb03
Alex Koshelev <koshelev@meta.com>
2022-11-03 22:11:29 -0700

    Secret share trait description

--- src/secret_sharing/mod.rs
+++ src/secret_sharing/mod.rs
@@ -7,6 +7,7 @@ pub use replicated::Replicated;
 use std::fmt::Debug;
 use std::ops::{Add, AddAssign, Mul, Neg, Sub, SubAssign};
 
+/// Secret share of a secret has additive and multiplicative properties.
 pub trait SecretShare<F>: Add + AddAssign + Neg + Sub + SubAssign + Mul<F> + Debug + Sized {}
 
 impl<F: Field> SecretShare<F> for Replicated<F> {}
