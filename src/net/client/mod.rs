thurstonsand/raw-ipa

revamp client, remove `Command`

src/net/client/mod.rs
@@ -1,54 +1,40 @@
-use super::Command;
-use crate::helpers::Identity;
-use crate::net::RecordHeaders;
-use crate::protocol::{QueryId, UniqueStepId};
-use async_trait::async_trait;
-use axum::body::Bytes;
-use axum::http::uri::{self, PathAndQuery};
-use axum::http::Request;
-use hyper::client::HttpConnector;
-use hyper::{Body, Client, Uri};
+mod error;
+
+pub use error::MpcClientError;
+
+use crate::{
+    helpers::Identity,
+    net::RecordHeaders,
+    protocol::{QueryId, UniqueStepId},
+};
+use axum::{
+    body::Bytes,
+    http::{
+        uri::{self, PathAndQuery},
+        Request,
+    },
+};
+use hyper::{client::HttpConnector, Body, Client, Uri};
 use hyper_tls::HttpsConnector;
-use thiserror::Error as ThisError;
 
-#[allow(dead_code)]
-#[derive(ThisError, Debug)]
-pub enum MpcClientError {
-    #[error("invalid host address")]
-    InvalidHostAddress(#[from] uri::InvalidUri),
-
-    #[error("network connection error")]
-    NetworkConnection(#[from] hyper::Error),
-
-    #[error("failed request: {0}")]
-    FailedRequest(hyper::StatusCode),
-
-    #[error(transparent)]
-    AxumError(#[from] axum::http::Error),
+pub struct HttpSendMessagesArgs<'a> {
+    pub query_id: &'a QueryId,
+    pub step: &'a UniqueStepId,
+    pub identity: Identity,
+    pub offset: u32,
+    pub data_size: u32,
+    pub messages: Bytes,
 }
 
-#[async_trait]
-pub trait MpcHandle {
-    async fn execute(&self, command: Command) -> Result<Vec<u8>, MpcClientError>;
-}
-
-pub struct MpcHttpConnection {
+#[allow(clippy::module_name_repetitions)] // follows standard naming convention
+#[derive(Clone)]
+pub struct MpcClient {
     client: Client<HttpsConnector<HttpConnector>>,
     scheme: uri::Scheme,
     authority: uri::Authority,
 }
 
-#[async_trait]
-impl MpcHandle for MpcHttpConnection {
-    async fn execute(&self, command: Command) -> Result<Vec<u8>, MpcClientError> {
-        match command {
-            Command::Echo(s) => self.echo(&s).await,
-        }
-    }
-}
-
-#[allow(dead_code)]
-impl MpcHttpConnection {
+impl MpcClient {
     /// addr must have a valid scheme and authority
     /// # Panics
     /// if addr does not have scheme and authority
@@ -57,9 +43,7 @@ impl MpcHttpConnection {
         // this works for both http and https
         let https = HttpsConnector::new();
         let client = Client::builder().build::<_, Body>(https);
-
         let parts = addr.into_parts();
-
         Self {
             client,
             scheme: parts.scheme.unwrap(),
@@ -74,6 +58,10 @@ impl MpcHttpConnection {
         Ok(Self::new(addr.parse()?))
     }
 
+    pub fn origin(&self) -> String {
+        format!("{}://{}", self.scheme.as_str(), self.authority.as_str())
+    }
+
     fn build_uri<T>(&self, p_and_q: T) -> Result<Uri, MpcClientError>
     where
         PathAndQuery: TryFrom<T>,
@@ -86,16 +74,26 @@ impl MpcHttpConnection {
             .build()?)
     }
 
-    async fn echo(&self, s: &str) -> Result<Vec<u8>, MpcClientError> {
+    /// Responds with whatever input is passed to it
+    /// # Errors
+    /// If the request has illegal arguments, or fails to deliver to helper
+    pub async fn echo(&self, s: &str) -> Result<Vec<u8>, MpcClientError> {
         let uri = self.build_uri(format!("/echo?foo={}", s))?;
 
         let response = self.client.get(uri).await?;
         let result = hyper::body::to_bytes(response.into_body()).await?;
-
         Ok(result.to_vec())
     }
 
-    async fn mul(&self, args: HttpMulArgs<'_>) -> Result<(), MpcClientError> {
+    /// Sends a batch of messages to another helper. Messages are a contiguous block of records in
+    /// some state of transformation within a protocol. Also includes ['`RecordId`] information and
+    /// [`ChannelId`].
+    /// # Errors
+    /// If the request has illegal arguments, or fails to deliver to helper
+    pub async fn send_messages(
+        &self,
+        args: HttpSendMessagesArgs<'_>,
+    ) -> Result<(), MpcClientError> {
         let uri = self.build_uri(format!(
             "/mul/query-id/{}/step/{}?identity={}",
             args.query_id.as_ref(),
@@ -112,21 +110,13 @@ impl MpcHttpConnection {
             .add_to(Request::post(uri))
             .body(Body::from(args.messages))?;
         let response = self.client.request(req).await?;
-        let resp_status = response.status();
-        resp_status
-            .is_success()
-            .then_some(())
-            .ok_or(MpcClientError::FailedRequest(resp_status))
+        let status = response.status();
+        if status.is_success() {
+            Ok(())
+        } else {
+            Err(MpcClientError::from_failed_resp(response).await)
+        }
     }
-}
-
-pub struct HttpMulArgs<'a> {
-    pub query_id: &'a QueryId,
-    pub step: &'a UniqueStepId,
-    pub identity: Identity,
-    pub offset: u32,
-    pub data_size: u32,
-    pub messages: Bytes,
 }
 
 #[cfg(test)]
@@ -137,7 +127,7 @@ mod tests {
     use hyper_tls::native_tls::TlsConnector;
     use tokio::sync::mpsc;
 
-    async fn mul_req(client: MpcHttpConnection, mut rx: mpsc::Receiver<MessageChunks>) {
+    async fn mul_req(client: MpcClient, mut rx: mpsc::Receiver<MessageChunks>) {
         const DATA_SIZE: u32 = 4;
         const DATA_LEN: u32 = 3;
         let query_id = QueryId;
@@ -146,8 +136,8 @@ mod tests {
         let offset = 0;
         let messages = &[0; (DATA_SIZE * DATA_LEN) as usize];
 
-        let res = client
-            .mul(HttpMulArgs {
+        client
+            .send_messages(HttpSendMessagesArgs {
                 query_id: &query_id,
                 step: &step,
                 identity,
@@ -155,8 +145,8 @@ mod tests {
                 data_size: DATA_SIZE,
                 messages: Bytes::from_static(messages),
             })
-            .await;
-        assert!(res.is_ok(), "{}", res.unwrap_err());
+            .await
+            .expect("send should succeed");
 
         let channel_id = ChannelId { identity, step };
         let env = [0; DATA_SIZE as usize].to_vec().into_boxed_slice();
@@ -181,7 +171,7 @@ mod tests {
 
         // setup client
         let client =
-            MpcHttpConnection::with_str_addr(&format!("http://localhost:{}", addr.port())).unwrap();
+            MpcClient::with_str_addr(&format!("http://localhost:{}", addr.port())).unwrap();
 
         // test
         mul_req(client, rx).await;
@@ -209,7 +199,7 @@ mod tests {
         http.enforce_http(false);
         let https = HttpsConnector::<HttpConnector>::from((http, conn.into()));
         let hyper_client = hyper::Client::builder().build(https);
-        let client = MpcHttpConnection {
+        let client = MpcClient {
             client: hyper_client,
             scheme: uri::Scheme::HTTPS,
             authority: uri::Authority::try_from(format!("localhost:{}", addr.port())).unwrap(),

private-attribution/ipa

src/net/client/mod.rs
@@ -1,54 +1,40 @@
-use super::Command;
-use crate::helpers::Role;
-use crate::net::RecordHeaders;
-use crate::protocol::{QueryId, UniqueStepId};
-use async_trait::async_trait;
-use axum::body::Bytes;
-use axum::http::uri::{self, PathAndQuery};
-use axum::http::Request;
-use hyper::client::HttpConnector;
-use hyper::{Body, Client, Uri};
+mod error;
+
+pub use error::MpcClientError;
+
+use crate::{
+    helpers::Role,
+    net::RecordHeaders,
+    protocol::{QueryId, UniqueStepId},
+};
+use axum::{
+    body::Bytes,
+    http::{
+        uri::{self, PathAndQuery},
+        Request,
+    },
+};
+use hyper::{client::HttpConnector, Body, Client, Uri};
 use hyper_tls::HttpsConnector;
-use thiserror::Error as ThisError;
 
-#[allow(dead_code)]
-#[derive(ThisError, Debug)]
-pub enum MpcClientError {
-    #[error("invalid host address")]
-    InvalidHostAddress(#[from] uri::InvalidUri),
-
-    #[error("network connection error")]
-    NetworkConnection(#[from] hyper::Error),
-
-    #[error("failed request: {0}")]
-    FailedRequest(hyper::StatusCode),
-
-    #[error(transparent)]
-    AxumError(#[from] axum::http::Error),
+pub struct HttpSendMessagesArgs<'a> {
+    pub query_id: &'a QueryId,
+    pub step: &'a UniqueStepId,
+    pub role: Role,
+    pub offset: u32,
+    pub data_size: u32,
+    pub messages: Bytes,
 }
 
-#[async_trait]
-pub trait MpcHandle {
-    async fn execute(&self, command: Command) -> Result<Vec<u8>, MpcClientError>;
-}
-
-pub struct MpcHttpConnection {
+#[allow(clippy::module_name_repetitions)] // follows standard naming convention
+#[derive(Clone)]
+pub struct MpcClient {
     client: Client<HttpsConnector<HttpConnector>>,
     scheme: uri::Scheme,
     authority: uri::Authority,
 }
 
-#[async_trait]
-impl MpcHandle for MpcHttpConnection {
-    async fn execute(&self, command: Command) -> Result<Vec<u8>, MpcClientError> {
-        match command {
-            Command::Echo(s) => self.echo(&s).await,
-        }
-    }
-}
-
-#[allow(dead_code)]
-impl MpcHttpConnection {
+impl MpcClient {
     /// addr must have a valid scheme and authority
     /// # Panics
     /// if addr does not have scheme and authority
@@ -57,9 +43,7 @@ impl MpcHttpConnection {
         // this works for both http and https
         let https = HttpsConnector::new();
         let client = Client::builder().build::<_, Body>(https);
-
         let parts = addr.into_parts();
-
         Self {
             client,
             scheme: parts.scheme.unwrap(),
@@ -74,6 +58,10 @@ impl MpcHttpConnection {
         Ok(Self::new(addr.parse()?))
     }
 
+    pub fn origin(&self) -> String {
+        format!("{}://{}", self.scheme.as_str(), self.authority.as_str())
+    }
+
     fn build_uri<T>(&self, p_and_q: T) -> Result<Uri, MpcClientError>
     where
         PathAndQuery: TryFrom<T>,
@@ -86,16 +74,26 @@ impl MpcHttpConnection {
             .build()?)
     }
 
-    async fn echo(&self, s: &str) -> Result<Vec<u8>, MpcClientError> {
+    /// Responds with whatever input is passed to it
+    /// # Errors
+    /// If the request has illegal arguments, or fails to deliver to helper
+    pub async fn echo(&self, s: &str) -> Result<Vec<u8>, MpcClientError> {
         let uri = self.build_uri(format!("/echo?foo={}", s))?;
 
         let response = self.client.get(uri).await?;
         let result = hyper::body::to_bytes(response.into_body()).await?;
-
         Ok(result.to_vec())
     }
 
-    async fn mul(&self, args: HttpMulArgs<'_>) -> Result<(), MpcClientError> {
+    /// Sends a batch of messages to another helper. Messages are a contiguous block of records in
+    /// some state of transformation within a protocol. Also includes ['`RecordId`] information and
+    /// [`ChannelId`].
+    /// # Errors
+    /// If the request has illegal arguments, or fails to deliver to helper
+    pub async fn send_messages(
+        &self,
+        args: HttpSendMessagesArgs<'_>,
+    ) -> Result<(), MpcClientError> {
         let uri = self.build_uri(format!(
             "/mul/query-id/{}/step/{}?role={}",
             args.query_id.as_ref(),
@@ -112,32 +110,25 @@ impl MpcHttpConnection {
             .add_to(Request::post(uri))
             .body(Body::from(args.messages))?;
         let response = self.client.request(req).await?;
-        let resp_status = response.status();
-        resp_status
-            .is_success()
-            .then_some(())
-            .ok_or(MpcClientError::FailedRequest(resp_status))
+        let status = response.status();
+        if status.is_success() {
+            Ok(())
+        } else {
+            Err(MpcClientError::from_failed_resp(response).await)
+        }
     }
-}
-
-pub struct HttpMulArgs<'a> {
-    pub query_id: &'a QueryId,
-    pub step: &'a UniqueStepId,
-    pub role: Role,
-    pub offset: u32,
-    pub data_size: u32,
-    pub messages: Bytes,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::helpers::fabric::{ChannelId, MessageChunks, MessageEnvelope};
+    use crate::helpers::Role;
     use crate::net::{BindTarget, MpcServer};
     use hyper_tls::native_tls::TlsConnector;
     use tokio::sync::mpsc;
 
-    async fn mul_req(client: MpcHttpConnection, mut rx: mpsc::Receiver<MessageChunks>) {
+    async fn mul_req(client: MpcClient, mut rx: mpsc::Receiver<MessageChunks>) {
         const DATA_SIZE: u32 = 4;
         const DATA_LEN: u32 = 3;
         let query_id = QueryId;
@@ -146,8 +137,8 @@ mod tests {
         let offset = 0;
         let messages = &[0; (DATA_SIZE * DATA_LEN) as usize];
 
-        let res = client
-            .mul(HttpMulArgs {
+        client
+            .send_messages(HttpSendMessagesArgs {
                 query_id: &query_id,
                 step: &step,
                 role,
@@ -155,8 +146,8 @@ mod tests {
                 data_size: DATA_SIZE,
                 messages: Bytes::from_static(messages),
             })
-            .await;
-        assert!(res.is_ok(), "{}", res.unwrap_err());
+            .await
+            .expect("send should succeed");
 
         let channel_id = ChannelId { role, step };
         let env = [0; DATA_SIZE as usize].to_vec().into_boxed_slice();
@@ -181,7 +172,7 @@ mod tests {
 
         // setup client
         let client =
-            MpcHttpConnection::with_str_addr(&format!("http://localhost:{}", addr.port())).unwrap();
+            MpcClient::with_str_addr(&format!("http://localhost:{}", addr.port())).unwrap();
 
         // test
         mul_req(client, rx).await;
@@ -209,7 +200,7 @@ mod tests {
         http.enforce_http(false);
         let https = HttpsConnector::<HttpConnector>::from((http, conn.into()));
         let hyper_client = hyper::Client::builder().build(https);
-        let client = MpcHttpConnection {
+        let client = MpcClient {
             client: hyper_client,
             scheme: uri::Scheme::HTTPS,
             authority: uri::Authority::try_from(format!("localhost:{}", addr.port())).unwrap(),