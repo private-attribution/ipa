64eadc64e8bc805e228f58bb985885d39155b9ea
Ben Savage <btsavage@fb.com>
2022-11-14 23:27:16 +0800

    Use vec::swap not mem::swap

--- src/protocol/sort/apply.rs
+++ src/protocol/sort/apply.rs
@@ -1,6 +1,5 @@
 use bitvec::bitvec;
 use embed_doc_image::embed_doc_image;
-use std::mem;
 
 #[embed_doc_image("apply", "images/sort/apply.png")]
 #[embed_doc_image("apply_inv", "images/sort/apply_inv.png")]
@@ -8,23 +7,20 @@ use std::mem;
 /// Permutation reorders (1, 2, . . . , m) into (σ(1), σ(2), . . . , σ(m)).
 /// For example, if σ(1) = 2, σ(2) = 3, σ(3) = 1, and σ(4) = 0, an input (A, B, C, D) is reordered into (C, D, B, A) by σ.
 /// ![Apply steps][apply]
-pub fn apply<T: Copy + Default>(permutation: &[u32], values: &mut [T]) {
+pub fn apply<T>(permutation: &[u32], values: &mut [T]) {
     debug_assert!(permutation.len() == values.len());
     let mut permuted = bitvec![0; permutation.len()];
-    let mut tmp: T = T::default();
 
     for i in 0..permutation.len() {
         if !permuted[i] {
-            mem::swap(&mut tmp, &mut values[i]);
             let mut pos_i = i;
-            let mut pos_j: usize = permutation[pos_i] as usize;
+            let mut pos_j = permutation[pos_i] as usize;
             while pos_j != i {
-                values[pos_i] = values[pos_j];
+                values.swap(pos_i, pos_j);
+                permuted.set(pos_j, true);
                 pos_i = pos_j;
                 pos_j = permutation[pos_i] as usize;
-                permuted.set(pos_i, true);
             }
-            mem::swap(&mut values[pos_i], &mut tmp);
             permuted.set(i, true);
         }
     }
@@ -34,20 +30,17 @@ pub fn apply<T: Copy + Default>(permutation: &[u32], values: &mut [T]) {
 /// is moved by `apply_inv` to be the σ(i)-th item. Therefore, if σ(1) = 2, σ(2) = 3, σ(3) = 1, and σ(4) = 0, an input (A, B, C, D) is
 /// reordered into (D, C, A, B).
 /// ![Apply inv steps][apply_inv]
-pub fn apply_inv<T: Copy + Default>(permutation: &[u32], values: &mut [T]) {
+pub fn apply_inv<T>(permutation: &[u32], values: &mut [T]) {
     let mut permuted = bitvec![0; permutation.len()];
-    let mut tmp: T;
 
     for i in 0..permutation.len() {
         if !permuted[i] {
-            let mut destination: usize = permutation[i] as usize;
-            tmp = values[i];
+            let mut destination = permutation[i] as usize;
             while destination != i {
-                mem::swap(&mut tmp, &mut values[destination]);
+                values.swap(i, destination);
                 permuted.set(destination, true);
                 destination = permutation[destination] as usize;
             }
-            mem::swap(&mut values[i], &mut tmp);
             permuted.set(i, true);
         }
     }
@@ -56,6 +49,7 @@ pub fn apply_inv<T: Copy + Default>(permutation: &[u32], values: &mut [T]) {
 #[cfg(test)]
 mod tests {
     use super::{apply, apply_inv};
+    use rand::seq::SliceRandom;
 
     #[test]
     fn apply_just_one_cycle() {
@@ -109,6 +103,32 @@ mod tests {
         let expected_output_apply = ["B", "A", "C", "J", "K", "D", "E", "F", "G", "H", "I"];
         apply_inv(&permutation, &mut values);
         assert_eq!(values, expected_output_apply);
+    }
+
+    #[test]
+    fn permutations_a_million_long() {
+        const SUPER_LONG: usize = 16 * 16 * 16 * 16 * 16; // 1,048,576, a bit over a million
+        let mut original_values = Vec::with_capacity(SUPER_LONG);
+        for i in 0..SUPER_LONG {
+            original_values.push(format!("{:#07x}", i));
+        }
+        let mut permutation: Vec<u32> = (0..SUPER_LONG)
+            .map(|i| usize::try_into(i).unwrap())
+            .collect();
+        let mut rng = rand::thread_rng();
+        permutation.shuffle(&mut rng);
+
+        let mut after_apply = original_values.clone();
+        apply(&permutation, &mut after_apply);
+        for i in 0..SUPER_LONG {
+            assert_eq!(after_apply[i], original_values[permutation[i] as usize]);
+        }
+
+        let mut after_apply_inv = original_values.clone();
+        apply_inv(&permutation, &mut after_apply_inv);
+        for i in 0..SUPER_LONG {
+            assert_eq!(original_values[i], after_apply_inv[permutation[i] as usize]);
+        }
     }
 
     #[test]
