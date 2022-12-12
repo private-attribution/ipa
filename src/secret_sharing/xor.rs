c87c566fc0edc3c576e86cce3ba107895f64c59e
Taiki Yamaguchi <taiki.yamaguchi@gmail.com>
2022-12-12 12:43:08 +0800

    Use BitArray instead of BitVec

--- src/secret_sharing/xor.rs
+++ src/secret_sharing/xor.rs
@@ -27,6 +27,9 @@ impl XorReplicated {
     }
 }
 
+/// Bit store size of `XorSecretShare` in bytes.
+const BIT_STORE_SIZE_IN_BYTES: usize = 4;
+
 /// XOR secret share.
 ///
 /// Bits are stored in the Little-Endian format. Accessing the first element
@@ -36,7 +39,7 @@ impl XorReplicated {
 /// ```
 /// use raw_ipa::secret_sharing::XorSecretShare;
 ///
-/// let s = XorSecretShare::<64>::from(2_u128);
+/// let s = XorSecretShare::from(2_u128);
 /// let b0 = s[0];
 /// let b1 = s[1];
 ///
@@ -44,29 +47,15 @@ impl XorReplicated {
 /// assert_eq!(b1, 1);
 /// ```
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct XorSecretShare<const N: usize>(BitVec<u8, Lsb0>);
+pub struct XorSecretShare(BitArray<[u8; BIT_STORE_SIZE_IN_BYTES], Lsb0>);
 
-// BitVec uses `usize` as its default store type in memory. Using `u8`
-// here allows us to store bits larger than 32 but less than 64 most
-// effectively (i.e., 40-bit = 5 * `u8` blocks).
-
-impl<const N: usize> XorSecretShare<N> {
-    const BITS: usize = N;
-    const SIZE_IN_BYTES: usize = Self::BITS / 8;
-
+impl XorSecretShare {
+    const SIZE_IN_BYTES: usize = BIT_STORE_SIZE_IN_BYTES;
     #[allow(dead_code)]
-    #[must_use]
-    pub fn new(v: BitVec<u8, Lsb0>) -> Self {
-        Self(v)
-    }
-
-    #[must_use]
-    pub fn zero() -> Self {
-        Self(BitVec::from_element(0))
-    }
+    const ZERO: Self = Self(BitArray::<[u8; Self::SIZE_IN_BYTES], Lsb0>::ZERO);
 }
 
-impl<const N: usize> BitXor for XorSecretShare<N> {
+impl BitXor for XorSecretShare {
     type Output = Self;
 
     fn bitxor(mut self, rhs: Self) -> Self::Output {
@@ -75,24 +64,23 @@ impl<const N: usize> BitXor for XorSecretShare<N> {
     }
 }
 
-impl<const N: usize> BitXorAssign for XorSecretShare<N> {
+impl BitXorAssign for XorSecretShare {
     fn bitxor_assign(&mut self, rhs: Self) {
         *self.0.as_mut_bitslice() ^= rhs.0;
     }
 }
 
-/// Converts from `u128` to `XorSecretShare`. If you wish to create a share
-/// that holds more than 128 bits, use `XorSecretShare::new()` with `bitvec!`
-/// instead.
-impl<const N: usize, T: Into<u128>> From<T> for XorSecretShare<N> {
+impl<T: Into<u128>> From<T> for XorSecretShare {
     fn from(v: T) -> Self {
-        Self(BitVec::from_slice(
-            &v.into().to_le_bytes()[0..Self::SIZE_IN_BYTES],
+        Self(BitArray::<_, Lsb0>::new(
+            v.into().to_le_bytes()[0..Self::SIZE_IN_BYTES]
+                .try_into()
+                .unwrap(),
         ))
     }
 }
 
-impl<const N: usize> Index<usize> for XorSecretShare<N> {
+impl Index<usize> for XorSecretShare {
     type Output = u8;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -104,23 +92,15 @@ impl<const N: usize> Index<usize> for XorSecretShare<N> {
     }
 }
 
-impl<const N: usize> Default for XorSecretShare<N> {
-    fn default() -> Self {
-        XorSecretShare::zero()
-    }
-}
-
 #[cfg(all(test, not(feature = "shuttle")))]
 mod tests {
+    use crate::secret_sharing::xor::BIT_STORE_SIZE_IN_BYTES;
+
     use super::XorSecretShare;
     use bitvec::prelude::*;
     use rand::{thread_rng, Rng};
 
-    fn secret_share(
-        a: u128,
-        b: u128,
-        c: u128,
-    ) -> (XorSecretShare<40>, XorSecretShare<40>, XorSecretShare<40>) {
+    fn secret_share(a: u128, b: u128, c: u128) -> (XorSecretShare, XorSecretShare, XorSecretShare) {
         (
             XorSecretShare::from(a),
             XorSecretShare::from(b),
@@ -130,31 +110,29 @@ mod tests {
 
     #[test]
     pub fn basic() {
+        assert_eq!(XorSecretShare::ZERO.0, bitarr!(u32, Lsb0; 0));
         assert_eq!(
-            XorSecretShare::<8>::zero().0.as_bitslice(),
-            bits![0, 0, 0, 0, 0, 0, 0, 0],
+            XorSecretShare::from(1_u128).0.as_bitslice(),
+            bits![
+                1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0
+            ],
         );
-
         assert_eq!(
-            XorSecretShare::<8>::from(1_u128).0.as_bitslice(),
-            bits![1, 0, 0, 0, 0, 0, 0, 0],
-        );
-
-        assert_eq!(
-            XorSecretShare::<8>::from(65793_u128).0.as_bitslice(),
-            bits![1, 0, 0, 0, 0, 0, 0, 0],
-        );
-
-        assert_eq!(
-            XorSecretShare::<24>::from(65793_u128).0.as_bitslice(),
-            bits![1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+            XorSecretShare::from((1_u128 << (BIT_STORE_SIZE_IN_BYTES * 8)) + 1)
+                .0
+                .as_bitslice(),
+            bits![
+                1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0
+            ],
         );
     }
 
     #[test]
     pub fn index() {
         let byte: u8 = 0b1010_1010;
-        let s = XorSecretShare::<8>::from(byte);
+        let s = XorSecretShare::from(byte);
         assert_eq!(s[0], 0);
         assert_eq!(s[1], 1);
         assert_eq!(s[2], 0);
