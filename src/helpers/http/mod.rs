82768b53835943a87826826aa068a591c79dcff8
Alex Koshelev <koshelev@meta.com>
2022-12-13 10:19:51 -0800

    Fix IntoShares input

--- src/helpers/http/mod.rs
+++ src/helpers/http/mod.rs
@@ -130,12 +130,13 @@ impl<'p> HttpHelper<'p> {
 #[cfg(test)]
 mod e2e_tests {
     use super::*;
+    use crate::secret_sharing::IntoShares;
     use crate::{
         ff::Fp31,
         helpers::SendBufferConfig,
         net::discovery,
         protocol::{basics::mul::SecureMul, context::Context, prss::SharedRandomness, RecordId},
-        test_fixture::{logging, IntoShares, Reconstruct},
+        test_fixture::{logging, Reconstruct},
     };
     use rand::rngs::mock::StepRng;
     use rand::rngs::StdRng;
