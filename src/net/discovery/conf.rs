820504cc36d6c1456e2a02f7a356caeea656224f
Alex Koshelev <koshelev@meta.com>
2022-12-14 14:17:10 -0800

    Address feedback

--- src/net/discovery/conf.rs
+++ src/net/discovery/conf.rs
@@ -32,16 +32,15 @@ impl PeerDiscovery for Conf {
 
 #[cfg(all(test, not(feature = "shuttle")))]
 mod tests {
-    use crate::helpers::Role;
     use crate::test_fixture::net::localhost_config;
     use hyper::Uri;
 
-    const H1_PUBLIC_KEY: &str = "13ccf4263cecbc30f50e6a8b9c8743943ddde62079580bc0b9019b05ba8fe924";
-    const H2_PUBLIC_KEY: &str = "925bf98243cf70b729de1d75bf4fe6be98a986608331db63902b82a1691dc13b";
-    const H3_PUBLIC_KEY: &str = "12c09881a1c7a92d1c70d9ea619d7ae0684b9cb45ecc207b98ef30ec2160a074";
-    const H1_URI: &str = "http://localhost:3000/";
-    const H2_URI: &str = "http://localhost:3001/";
-    const H3_URI: &str = "http://localhost:3002/";
+    const PUBLIC_KEY_1: &str = "13ccf4263cecbc30f50e6a8b9c8743943ddde62079580bc0b9019b05ba8fe924";
+    const PUBLIC_KEY_2: &str = "925bf98243cf70b729de1d75bf4fe6be98a986608331db63902b82a1691dc13b";
+    const PUBLIC_KEY_3: &str = "12c09881a1c7a92d1c70d9ea619d7ae0684b9cb45ecc207b98ef30ec2160a074";
+    const URI_1: &str = "http://localhost:3000/";
+    const URI_2: &str = "http://localhost:3001/";
+    const URI_3: &str = "http://localhost:3002/";
 
     fn hex_str_to_public_key(hex_str: &str) -> x25519_dalek::PublicKey {
         let pk_bytes: [u8; 32] = hex::decode(hex_str)
@@ -55,25 +54,22 @@ mod tests {
     fn parse_config() {
         let conf = localhost_config([3000, 3001, 3002]);
 
-        // H1
-        assert_eq!(conf.peers[Role::H1].origin, H1_URI.parse::<Uri>().unwrap());
+        assert_eq!(conf.peers[0].origin, URI_1.parse::<Uri>().unwrap());
         assert_eq!(
-            conf.peers[Role::H1].tls.public_key,
-            hex_str_to_public_key(H1_PUBLIC_KEY)
+            conf.peers[0].tls.public_key,
+            hex_str_to_public_key(PUBLIC_KEY_1)
         );
 
-        // H2
-        assert_eq!(conf.peers[Role::H2].origin, H2_URI.parse::<Uri>().unwrap());
+        assert_eq!(conf.peers[1].origin, URI_2.parse::<Uri>().unwrap());
         assert_eq!(
-            conf.peers[Role::H2].tls.public_key,
-            hex_str_to_public_key(H2_PUBLIC_KEY)
+            conf.peers[1].tls.public_key,
+            hex_str_to_public_key(PUBLIC_KEY_2)
         );
 
-        // H3
-        assert_eq!(conf.peers[Role::H3].origin, H3_URI.parse::<Uri>().unwrap());
+        assert_eq!(conf.peers[2].origin, URI_3.parse::<Uri>().unwrap());
         assert_eq!(
-            conf.peers[Role::H3].tls.public_key,
-            hex_str_to_public_key(H3_PUBLIC_KEY)
+            conf.peers[2].tls.public_key,
+            hex_str_to_public_key(PUBLIC_KEY_3)
         );
     }
 }
