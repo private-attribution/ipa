4b6f9ca4f7b127d7f5493c21eee9f47d4a825a49
Taiki Yamaguchi <taiki.yamaguchi@gmail.com>
2022-12-16 16:33:57 +0800

    TryFrom and truncate_from

--- src/bits/bit_array.rs
+++ src/bits/bit_array.rs
@@ -18,7 +18,7 @@ type U8_8 = BitArr!(for 64, in u8, Lsb0);
 /// ```
 /// use raw_ipa::bits::BitArray64;
 ///
-/// let s = BitArray64::from(2_u128);
+/// let s = BitArray64::try_from(2_u128).unwrap();
 /// let b0 = s[0_usize];
 /// let b1 = s[1_usize];
 ///
@@ -30,9 +30,18 @@ pub struct BitArray64(U8_8);
 
 impl BitArray for BitArray64 {
     const SIZE_IN_BYTES: usize = std::mem::size_of::<Self>();
+    const BITS: u32 = 64;
 
     #[allow(dead_code)]
     const ZERO: Self = Self(U8_8::ZERO);
+
+    fn truncate_from<T: Into<u128>>(v: T) -> Self {
+        Self(U8_8::new(
+            v.into().to_le_bytes()[0..<Self as BitArray>::SIZE_IN_BYTES]
+                .try_into()
+                .unwrap(),
+        ))
+    }
 }
 
 impl BooleanOps for BitArray64 {}
@@ -83,13 +92,22 @@ impl Not for BitArray64 {
     }
 }
 
-impl<T: Into<u128>> From<T> for BitArray64 {
-    fn from(v: T) -> Self {
-        Self(U8_8::new(
-            v.into().to_le_bytes()[0..<Self as BitArray>::SIZE_IN_BYTES]
-                .try_into()
-                .unwrap(),
-        ))
+impl TryFrom<u128> for BitArray64 {
+    type Error = String;
+
+    /// Fallible conversion from `u128` to this data type. The input value must
+    /// be `Self::BITS` long. That is, leading 0's must be longer or equal to
+    /// `Self::BITS`, or it will return an error.
+    fn try_from(v: u128) -> Result<Self, Self::Error> {
+        if 128 - v.leading_zeros() <= Self::BITS {
+            Ok(Self::truncate_from(v))
+        } else {
+            Err(format!(
+                "Bit array size {} is too small to hold the value {}.",
+                Self::BITS,
+                v
+            ))
+        }
     }
 }
 
@@ -122,17 +140,24 @@ mod tests {
 
     #[test]
     pub fn basic() {
+        let one = bitarr!(u64, Lsb0; 1);
+
         assert_eq!(BitArray64::ZERO.0, bitarr!(u64, Lsb0; 0));
-        assert_eq!(BitArray64::from(1_u128).0, bitarr!(u64, Lsb0; 1));
+        assert_eq!(BitArray64::try_from(1_u128).unwrap().0, one);
+
+        let b65 = (1_u128 << BitArray64::BITS) + 1;
+        assert!(BitArray64::try_from(b65).is_err());
         assert_eq!(
-            BitArray64::from((1_u128 << (BitArray64::SIZE_IN_BYTES * 8)) + 1).0,
-            bitarr!(u64, Lsb0; 1)
+            BitArray64::try_from(b65 & u128::from(u64::MAX)).unwrap().0,
+            one
         );
+
+        assert_eq!(BitArray64::truncate_from(b65).0, one);
     }
 
     #[test]
     pub fn index() {
-        let s = BitArray64::from((1_u128 << (BitArray64::SIZE_IN_BYTES * 8)) + 1);
+        let s = BitArray64::try_from(1_u128).unwrap();
         assert_eq!(s[0_usize], 1);
         assert_eq!(s[63_u32], 0);
     }
@@ -140,7 +165,7 @@ mod tests {
     #[test]
     #[should_panic]
     pub fn out_of_count_index() {
-        let s = BitArray64::from(1_u128);
+        let s = BitArray64::try_from(1_u128).unwrap();
         // Below assert doesn't matter. The indexing should panic
         assert_eq!(s[64_usize], 0);
     }
@@ -152,13 +177,13 @@ mod tests {
             let a = rng.gen::<u128>();
             let b = rng.gen::<u128>();
 
-            let and = BitArray64::from(a & b);
-            let or = BitArray64::from(a | b);
-            let xor = BitArray64::from(a ^ b);
-            let not = BitArray64::from(!a);
+            let and = BitArray64::truncate_from(a & b);
+            let or = BitArray64::truncate_from(a | b);
+            let xor = BitArray64::truncate_from(a ^ b);
+            let not = BitArray64::truncate_from(!a);
 
-            let a = BitArray64::from(a);
-            let b = BitArray64::from(b);
+            let a = BitArray64::try_from(a & u128::from(u64::MAX)).unwrap();
+            let b = BitArray64::try_from(b & u128::from(u64::MAX)).unwrap();
 
             assert_eq!(a & b, and);
             assert_eq!(a | b, or);
