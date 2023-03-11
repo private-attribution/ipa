45edbca509219f9deb159faec4dc4ee4d8210ca7
Alex Koshelev <koshelev@meta.com>
2023-03-10 16:41:39 -0800

    Revert changes in `Serializable` traint

--- src/bits/mod.rs
+++ src/bits/mod.rs
@@ -60,22 +60,18 @@ impl<T> BooleanRefOps for T where
 }
 
 /// Trait for items that have fixed-byte length representation.
-pub trait Serializable {
+pub trait Serializable : Sized {
     /// Required number of bytes to store this message on disk/network
     type Size: ArrayLength<u8>;
 
     /// Serialize this message to a mutable slice. It is enforced at compile time or on the caller
     /// side that this slice is sized to fit this instance. Implementations do not need to check
     /// the buffer size.
-    fn serialize(self, buf: &mut GenericArray<u8, Self::Size>)
-    where
-        Self: Sized;
+    fn serialize(self, buf: &mut GenericArray<u8, Self::Size>);
 
     /// Deserialize message from a sequence of bytes. Similar to [`serialize`], it is enforced that
     /// buffer has enough capacity to fit instances of this trait.
     ///
     /// [`serialize`]: Self::serialize
-    fn deserialize(buf: &GenericArray<u8, Self::Size>) -> Self
-    where
-        Self: Sized;
+    fn deserialize(buf: &GenericArray<u8, Self::Size>) -> Self;
 }
