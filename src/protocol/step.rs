4c3e7ec22d2ac107f1e57314c9582fc7d0076aef
Taiki Yamaguchi <taiki.yamaguchi@gmail.com>
2023-05-12 20:53:35 +0900

    Print the step's full path

--- src/protocol/step.rs
+++ src/protocol/step.rs
@@ -74,9 +74,14 @@ impl<S: Step + ?Sized> StepNarrow<S> for Descriptive {
             assert!(!s.contains('/'), "The string for a step cannot contain '/'");
         }
 
-        Self {
-            id: self.id.clone() + "/" + step.as_ref(),
+        let mut id = self.id.clone() + "/";
+        #[cfg(feature = "step-trace")]
+        {
+            id += &(std::any::type_name::<S>().to_owned() + "::");
         }
+        id += step.as_ref();
+
+        Self { id }
     }
 }
 
