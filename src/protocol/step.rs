64e5102a318d5fa825072b3fb704a7631ae51697
Taiki Yamaguchi <taiki.yamaguchi@gmail.com>
2023-05-18 10:19:37 +0900

    gate the step trace logging to in-memory-infra

--- src/protocol/step.rs
+++ src/protocol/step.rs
@@ -75,7 +75,7 @@ impl<S: Step + ?Sized> StepNarrow<S> for Descriptive {
         }
 
         let mut id = self.id.clone() + "/";
-        #[cfg(feature = "step-trace")]
+        #[cfg(all(feature = "step-trace", feature = "in-memory-infra"))]
         {
             id += &(std::any::type_name::<S>().to_owned() + "::");
         }
