4fe4c3eff8fe155ea4c5a52e3ba31f9af2760e5f
Erik Taubeneck <erik.taubeneck@gmail.com>
2023-08-16 11:48:01 -0700

    remove duplicated assert_stream_send

--- src/query/runner/aggregate.rs
+++ src/query/runner/aggregate.rs
@@ -1,7 +1,5 @@
 use std::marker::PhantomData;
 
-use futures::Stream;
-
 use crate::{
     error::Error,
     ff::{Gf2, PrimeField, Serializable},
@@ -71,14 +69,4 @@ where
     ) -> Result<Vec<Replicated<F>>, Error> {
         todo!()
     }
-}
-
-/// Helps to convince the compiler that things are `Send`. Like `seq_join::assert_send`, but for
-/// streams.
-///
-/// <https://github.com/rust-lang/rust/issues/102211#issuecomment-1367900125>
-pub fn _assert_stream_send<'a, T>(
-    st: impl Stream<Item = T> + Send + 'a,
-) -> impl Stream<Item = T> + Send + 'a {
-    st
 }
