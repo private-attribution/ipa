ccb9d091a16a18618dba0280243756d2456e5673
Andy Leiserson <aleiserson@mozilla.com>
2024-03-19 17:17:15 -0700

    CI fixes

--- ipa-core/src/protocol/context/validator.rs
+++ ipa-core/src/protocol/context/validator.rs
@@ -14,15 +14,10 @@ use crate::{
         prss::SharedRandomness,
         RecordId,
     },
-    secret_sharing::{
-        replicated::{
-            malicious::{
-                AdditiveShare as MaliciousReplicated, DowngradeMalicious, ExtendableField,
-            },
-            semi_honest::AdditiveShare as Replicated,
-            ReplicatedSecretSharing,
-        },
-        SharedValue,
+    secret_sharing::replicated::{
+        malicious::{AdditiveShare as MaliciousReplicated, DowngradeMalicious, ExtendableField},
+        semi_honest::AdditiveShare as Replicated,
+        ReplicatedSecretSharing,
     },
     sync::{Mutex, Weak},
 };
@@ -32,6 +27,7 @@ use crate::{
     protocol::basics::Reveal,
     protocol::context::Context,
     protocol::context::{MaliciousContext, UpgradedMaliciousContext},
+    secret_sharing::SharedValue,
     sync::Arc,
 };
 
