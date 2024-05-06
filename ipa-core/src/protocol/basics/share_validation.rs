b2771f6c6d86d19bf5263536ffc611c58f1d9078
danielmasny <daniel.masny@rub.de>
2024-05-06 16:34:45 -0700

    add tests, change input type

--- ipa-core/src/protocol/basics/share_validation.rs
+++ ipa-core/src/protocol/basics/share_validation.rs
@@ -2,12 +2,11 @@ use futures_util::future::try_join;
 
 use crate::{
     error::Error,
-    helpers::{Direction, MpcReceivingEnd, SendingEnd},
-    protocol::{
-        context::Context,
-        ipa_prf::malicious_security::hashing::{compute_hash, Hash},
-        RecordId,
+    helpers::{
+        hashing::{compute_hash, Hash},
+        Direction,
     },
+    protocol::{context::Context, RecordId},
     secret_sharing::SharedValue,
 };
 
@@ -24,34 +23,32 @@ use crate::{
 ///
 /// # Errors
 /// propagates errors from send and receive
-pub async fn validate_replicated_shares<C, S>(
+pub async fn validate_replicated_shares<'a, 'b, C, I, J, S>(
     ctx: C,
-    input_left: &[S],
-    input_right: &[S],
+    input_left: I,
+    input_right: J,
 ) -> Result<(), Error>
 where
     C: Context,
+    I: IntoIterator<Item = &'a S>,
+    J: IntoIterator<Item = &'b S>,
     S: SharedValue,
 {
-    // compute hash of `left.neg` and `right`
+    // compute hash of `left`
     let hash_left = compute_hash(input_left);
 
     // set up context
     let ctx_new = &(ctx.set_total_records(1usize));
     // set up channels
-    let send_channel: &SendingEnd<_, Hash> =
-        &ctx_new.send_channel(ctx.role().peer(Direction::Right));
-    let receive_channel: &MpcReceivingEnd<Hash> =
-        &ctx_new.recv_channel(ctx.role().peer(Direction::Left));
+    let send_channel = &ctx_new.send_channel::<Hash>(ctx.role().peer(Direction::Right));
+    let receive_channel = &ctx_new.recv_channel::<Hash>(ctx.role().peer(Direction::Left));
 
     let ((), hash_right) = try_join(
         // send hash
-        send_channel.send(RecordId::from(0usize), compute_hash(input_right)),
-        receive_channel.receive(RecordId::from(0usize)),
+        send_channel.send(RecordId::FIRST, compute_hash(input_right)),
+        receive_channel.receive(RecordId::FIRST),
     )
     .await?;
-
-    debug_assert_eq!(hash_left, hash_right);
 
     if hash_left == hash_right {
         Ok(())
@@ -75,36 +72,44 @@ where
 ///
 /// # Errors
 /// propagates errors from `validate_replicated_shares`
-pub async fn validate_sum_to_zero<C, S>(
+pub async fn validate_sum_to_zero<'a, C, I, S>(
     ctx: C,
-    input_left: &[S],
-    input_right: &[S],
+    input_left: I,
+    input_right: I,
 ) -> Result<(), Error>
 where
     C: Context,
+    I: IntoIterator<Item = &'a S>,
     S: SharedValue,
 {
-    validate_replicated_shares(
-        ctx,
-        input_left,
-        &input_right.iter().map(|x| x.neg()).collect::<Vec<_>>(),
-    )
-    .await
+    // compute left.neg
+    let left_neg = input_left.into_iter().map(|x| x.neg()).collect::<Vec<_>>();
+
+    validate_replicated_shares(ctx, &left_neg, input_right).await
 }
 
 #[cfg(all(test, unit_test))]
 mod test {
     use std::ops::Neg;
 
+    use ipa_macros::Step;
     use rand::{thread_rng, Rng};
 
     use crate::{
-        ff::Fp61BitPrime,
-        protocol::basics::share_validation::validate_sum_to_zero,
+        error::Error,
+        ff::{Field, Fp61BitPrime},
+        protocol::{basics::share_validation::validate_sum_to_zero, context::Context},
         secret_sharing::{replicated::ReplicatedSecretSharing, SharedValue},
         test_executor::run,
         test_fixture::{Runner, TestWorld},
     };
+
+    #[derive(Step)]
+    pub(crate) enum Step {
+        Correctness,
+        Misaligned,
+        Changed,
+    }
 
     // Test sum to zero
     // by generating a vec of zero values
@@ -124,12 +129,36 @@ mod test {
             let _ = world
                 .semi_honest(r.into_iter(), |ctx, input| async move {
                     let r_right = input.iter().map(|x| x.right().neg()).collect::<Vec<_>>();
-                    let r_left = input
+                    let mut r_left = input
                         .iter()
                         .map(ReplicatedSecretSharing::left)
                         .collect::<Vec<_>>();
 
-                    validate_sum_to_zero(ctx, &r_left, &r_right).await.unwrap();
+                    validate_sum_to_zero(ctx.narrow(&Step::Correctness), &r_left, &r_right)
+                        .await
+                        .unwrap();
+
+                    // check misaligned causes error
+                    let error = validate_sum_to_zero(
+                        ctx.narrow(&Step::Misaligned),
+                        &r_left[0..len - 1],
+                        &r_right[1..len],
+                    )
+                    .await;
+
+                    assert!(matches!(error, Err(Error::InconsistentShares)));
+
+                    // check changing causes error
+                    r_left[5] += Fp61BitPrime::ONE;
+
+                    let error = validate_sum_to_zero(
+                        ctx.narrow(&Step::Changed),
+                        &r_left[0..len - 1],
+                        &r_right[1..len],
+                    )
+                    .await;
+
+                    assert!(matches!(error, Err(Error::InconsistentShares)));
                 })
                 .await;
         });
