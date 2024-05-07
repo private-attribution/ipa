e0c3317990c38456d8553b716cb04db22d6b1443
danielmasny <daniel.masny@rub.de>
2024-05-07 11:37:02 -0700

    renaming

--- ipa-core/src/protocol/basics/share_validation.rs
+++ ipa-core/src/protocol/basics/share_validation.rs
@@ -15,11 +15,11 @@ use crate::{
 /// the function verifies that `H1.x0 == H3.x0`, `H1.x1 == H2.x1`, `H2.x2 == H3.x2`
 ///
 /// We use a hash based approach that is secure in the random oracle model
-/// further, only one of left and right helper check that it is zero
+/// further, only one of left and right helper check the equality of the shares
 /// this is might not be sufficient in some applications to prevent malicious behavior
 ///
 /// The left helper simply hashes the vector and sends it to the right,
-/// the right helper negates his vector, hashes it and compares it to the received hash
+/// the right helper hashes his vector and compares it to the received hash
 ///
 /// # Errors
 /// propagates errors from send and receive
@@ -43,14 +43,14 @@ where
     let send_channel = ctx_new.send_channel::<Hash>(ctx.role().peer(Direction::Right));
     let receive_channel = ctx_new.recv_channel::<Hash>(ctx.role().peer(Direction::Left));
 
-    let ((), hash_right) = try_join(
+    let ((), hash_received) = try_join(
         // send hash
         send_channel.send(RecordId::FIRST, compute_hash(input_right)),
         receive_channel.receive(RecordId::FIRST),
     )
     .await?;
 
-    if hash_left == hash_right {
+    if hash_left == hash_received {
         Ok(())
     } else {
         Err(Error::InconsistentShares)
@@ -58,9 +58,9 @@ where
 }
 
 /// This function is similar to validate the consistency of shares with the difference
-/// that it validates that the shares sum to zero rather than being identical
-/// i.e. `H1` holds `(x0,x1)`, `H2` holds `(x1,x2)`, `H3` holds `(x2,x0)`
-/// the function verifies that `H1.x0 == -H3.x0`, `H1.x1 == -H2.x1`, `H2.x2 == -H3.x2`
+/// that it validates that tuple of shares sum to zero rather than being identical
+/// i.e. `H1` holds `(H1_x0,H1_x1)`, `H2` holds `(H2_x1,H2_x2)`, `H3` holds `(H3_x2,H3_x0)`
+/// the function verifies that `H1_x0 == -H3_x0`, `H1_x1 == -H2_x1`, `H2_x2 == -H3_x2`
 ///
 /// We use a hash based approach that is secure in the random oracle model
 /// further, only one of left and right helper check that it is zero
@@ -72,7 +72,7 @@ where
 ///
 /// # Errors
 /// propagates errors from `validate_replicated_shares`
-pub async fn validate_sum_to_zero<'a, C, I, S>(
+pub async fn validate_three_two_way_sharing_of_zero<'a, C, I, S>(
     ctx: C,
     input_left: I,
     input_right: I,
@@ -98,8 +98,10 @@ mod test {
     use crate::{
         error::Error,
         ff::{Field, Fp61BitPrime},
-        protocol::{basics::share_validation::validate_sum_to_zero, context::Context},
-        secret_sharing::{replicated::ReplicatedSecretSharing, SharedValue},
+        protocol::{
+            basics::share_validation::validate_three_two_way_sharing_of_zero, context::Context,
+        },
+        secret_sharing::replicated::ReplicatedSecretSharing,
         test_executor::run,
         test_fixture::{Runner, TestWorld},
     };
@@ -111,20 +113,22 @@ mod test {
         Changed,
     }
 
-    // Test sum to zero
-    // by generating a vec of zero values
-    // secret share it
-    // and run the sum to zero protocol
+    // Test three two way shares of zero
+    // we generated replicated shares of a vector of random values
+    // each helper party negates one of its shares
+    // we then check whether validate_three_two_way_sharing_of_zero succeeds
+    // we also test for failure when the shares are misaligned or one of them has been changed
     #[test]
-    fn validate_sum_to_zero_test() {
+    fn three_two_way_shares_of_zero() {
         run(|| async move {
             let world = TestWorld::default();
             let mut rng = thread_rng();
 
             let len: usize = rng.gen_range(50..100);
 
-            let mut r = vec![Fp61BitPrime::ZERO; len];
-            r.iter_mut().for_each(|x| *x = rng.gen());
+            let r = (0..len)
+                .map(|_| rng.gen::<Fp61BitPrime>())
+                .collect::<Vec<_>>();
 
             let _ = world
                 .semi_honest(r.into_iter(), |ctx, input| async move {
@@ -134,12 +138,16 @@ mod test {
                         .map(ReplicatedSecretSharing::left)
                         .collect::<Vec<_>>();
 
-                    validate_sum_to_zero(ctx.narrow(&Step::Correctness), &r_left, &r_right)
-                        .await
-                        .unwrap();
+                    validate_three_two_way_sharing_of_zero(
+                        ctx.narrow(&Step::Correctness),
+                        &r_left,
+                        &r_right,
+                    )
+                    .await
+                    .unwrap();
 
                     // check misaligned causes error
-                    let error = validate_sum_to_zero(
+                    let error = validate_three_two_way_sharing_of_zero(
                         ctx.narrow(&Step::Misaligned),
                         &r_left[0..len - 1],
                         &r_right[1..len],
@@ -151,7 +159,7 @@ mod test {
                     // check changing causes error
                     r_left[5] += Fp61BitPrime::ONE;
 
-                    let error = validate_sum_to_zero(
+                    let error = validate_three_two_way_sharing_of_zero(
                         ctx.narrow(&Step::Changed),
                         &r_left[0..len - 1],
                         &r_right[1..len],
