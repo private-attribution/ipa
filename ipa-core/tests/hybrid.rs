2e89098976ce0bbc63fa2b574107d60086d53143
Erik Taubeneck <erik.taubeneck@gmail.com>
2024-09-20 13:48:21 -0700

    remove test mod

--- ipa-core/tests/hybrid.rs
+++ ipa-core/tests/hybrid.rs
@@ -2,51 +2,47 @@
 #[allow(dead_code)]
 mod common;
 
-#[cfg(all(test, feature = "cli", feature = "test-fixture"))]
-mod tests {
-    use std::process::{Command, Stdio};
+use std::process::{Command, Stdio};
 
-    use rand::thread_rng;
-    use rand_core::RngCore;
+use common::{tempdir::TempDir, CommandExt, UnwrapStatusExt, TEST_RC_BIN};
+use rand::thread_rng;
+use rand_core::RngCore;
 
-    use super::common::{tempdir::TempDir, CommandExt, UnwrapStatusExt, TEST_RC_BIN};
+pub const IN_THE_CLEAR_BIN: &str = env!("CARGO_BIN_EXE_in_the_clear");
 
-    pub const IN_THE_CLEAR_BIN: &str = env!("CARGO_BIN_EXE_in_the_clear");
+// this currently only generates data and runs in the clear
+// eventaully we'll want to add the MPC as well
+#[test]
+fn test_hybrid() {
+    const INPUT_SIZE: usize = 100;
+    const MAX_CONVERSION_VALUE: usize = 5;
+    const MAX_BREAKDOWN_KEY: usize = 20;
+    const MAX_CONVS_PER_IMP: usize = 10;
 
-    // this currently only generates data and runs in the clear
-    // eventaully we'll want to add the MPC as well
-    #[test]
-    fn test_hybrid() {
-        const INPUT_SIZE: usize = 100;
-        const MAX_CONVERSION_VALUE: usize = 5;
-        const MAX_BREAKDOWN_KEY: usize = 20;
-        const MAX_CONVS_PER_IMP: usize = 10;
+    let dir = TempDir::new_delete_on_drop();
 
-        let dir = TempDir::new_delete_on_drop();
+    // Gen inputs
+    let input_file = dir.path().join("ipa_inputs.txt");
+    let output_file = dir.path().join("ipa_output.json");
 
-        // Gen inputs
-        let input_file = dir.path().join("ipa_inputs.txt");
-        let output_file = dir.path().join("ipa_output.json");
+    let mut command = Command::new(TEST_RC_BIN);
+    command
+        .args(["--output-file".as_ref(), input_file.as_os_str()])
+        .arg("gen-hybrid-inputs")
+        .args(["--count", &INPUT_SIZE.to_string()])
+        .args(["--max-conversion-value", &MAX_CONVERSION_VALUE.to_string()])
+        .args(["--max-breakdown-key", &MAX_BREAKDOWN_KEY.to_string()])
+        .args(["--max-convs-per-imp", &MAX_CONVS_PER_IMP.to_string()])
+        .args(["--seed", &thread_rng().next_u64().to_string()])
+        .silent()
+        .stdin(Stdio::piped());
+    command.status().unwrap_status();
 
-        let mut command = Command::new(TEST_RC_BIN);
-        command
-            .args(["--output-file".as_ref(), input_file.as_os_str()])
-            .arg("gen-hybrid-inputs")
-            .args(["--count", &INPUT_SIZE.to_string()])
-            .args(["--max-conversion-value", &MAX_CONVERSION_VALUE.to_string()])
-            .args(["--max-breakdown-key", &MAX_BREAKDOWN_KEY.to_string()])
-            .args(["--max-convs-per-imp", &MAX_CONVS_PER_IMP.to_string()])
-            .args(["--seed", &thread_rng().next_u64().to_string()])
-            .silent()
-            .stdin(Stdio::piped());
-        command.status().unwrap_status();
-
-        let mut command = Command::new(IN_THE_CLEAR_BIN);
-        command
-            .args(["--input-file".as_ref(), input_file.as_os_str()])
-            .args(["--output-file".as_ref(), output_file.as_os_str()])
-            .silent()
-            .stdin(Stdio::piped());
-        command.status().unwrap_status();
-    }
+    let mut command = Command::new(IN_THE_CLEAR_BIN);
+    command
+        .args(["--input-file".as_ref(), input_file.as_os_str()])
+        .args(["--output-file".as_ref(), output_file.as_os_str()])
+        .silent()
+        .stdin(Stdio::piped());
+    command.status().unwrap_status();
 }
