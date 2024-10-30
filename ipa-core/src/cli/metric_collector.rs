46b25e695f3f1a944a7ffd5210ea938b0617a336
Alex Koshelev <koshelev@meta.com>
2024-10-29 21:04:54 -0700

    Comment logging

--- ipa-core/src/cli/metric_collector.rs
+++ ipa-core/src/cli/metric_collector.rs
@@ -9,6 +9,7 @@ use crate::ff::curve_points::{COMPRESS_OP, COMPRESS_SER_OP, COMPRESS_FROM_FP_OP,
 pub struct CollectorHandle {
     thread_handle: JoinHandle<()>,
     producer: MetricsProducer,
+    pub _controller: MetricsCollectorController,
 }
 
 ///
@@ -21,63 +22,64 @@ pub fn install_collector() -> io::Result<CollectorHandle> {
     let (producer, controller, handle) =
         ipa_metrics::install_new_thread(MetricChannelType::Unbounded)?;
     tracing::info!("Metrics engine is enabled");
-    thread::spawn(|| {
-        tracing::info!("metric observer is started");
-        const METRICS_OF_INTEREST: [&'static str; 9] = [
-            COMPRESS_OP,
-            COMPRESS_SER_OP,
-            COMPRESS_HASH_OP,
-            COMPRESS_FROM_SCALAR_OP,
-            COMPRESS_FROM_FP_OP,
-            DECOMPRESS_OP,
-            DECOMPRESS_ADD_OP,
-            DECOMPRESS_MUL_OP,
-            DECOMPRESS_DESER_OP
-        ];
-
-        struct Watcher(MetricsCollectorController, HashMap<&'static str, u64>);
-
-        impl Watcher {
-            fn dump(&mut self) {
-                if let Ok(snapshot) = self.0.snapshot() {
-                    let mut dump = String::new();
-                    let mut needs_dump = false;
-                    for metric in METRICS_OF_INTEREST {
-                        let value = snapshot.counter_val(counter!(metric));
-                        if value > 0 && self.1.get(metric) != Some(&value) {
-                            self.1.insert(metric, value);
-                            needs_dump = true;
-                        }
-                        if needs_dump {
-                            dump += &format!("{metric}={value}\n");
-                        }
-                    }
-
-                    if dump.len() > 0 {
-                        tracing::info!("Metrics dump:\n{dump}")
-                    }
-                } else {
-                    tracing::error!("Failed to dump metrics")
-                }
-            }
-        }
-
-        impl Drop for Watcher {
-            fn drop(&mut self) {
-                tracing::info!("metric watcher is being dropped");
-                self.dump();
-            }
-        }
-
-        let mut w = Watcher(controller, HashMap::default());
-        loop {
-            thread::sleep(Duration::from_secs(10));
-            w.dump();
-        }
-    });
+    // thread::spawn(|| {
+    //     tracing::info!("metric observer is started");
+    //     const METRICS_OF_INTEREST: [&'static str; 9] = [
+    //         COMPRESS_OP,
+    //         COMPRESS_SER_OP,
+    //         COMPRESS_HASH_OP,
+    //         COMPRESS_FROM_SCALAR_OP,
+    //         COMPRESS_FROM_FP_OP,
+    //         DECOMPRESS_OP,
+    //         DECOMPRESS_ADD_OP,
+    //         DECOMPRESS_MUL_OP,
+    //         DECOMPRESS_DESER_OP
+    //     ];
+    //
+    //     struct Watcher(MetricsCollectorController, HashMap<&'static str, u64>);
+    //
+    //     impl Watcher {
+    //         fn dump(&mut self) {
+    //             if let Ok(snapshot) = self.0.snapshot() {
+    //                 let mut dump = String::new();
+    //                 let mut needs_dump = false;
+    //                 for metric in METRICS_OF_INTEREST {
+    //                     let value = snapshot.counter_val(counter!(metric));
+    //                     if value > 0 && self.1.get(metric) != Some(&value) {
+    //                         self.1.insert(metric, value);
+    //                         needs_dump = true;
+    //                     }
+    //                     if needs_dump {
+    //                         dump += &format!("{metric}={value}\n");
+    //                     }
+    //                 }
+    //
+    //                 if dump.len() > 0 {
+    //                     tracing::info!("Metrics dump:\n{dump}")
+    //                 }
+    //             } else {
+    //                 tracing::error!("Failed to dump metrics")
+    //             }
+    //         }
+    //     }
+    //
+    //     impl Drop for Watcher {
+    //         fn drop(&mut self) {
+    //             tracing::info!("metric watcher is being dropped");
+    //             self.dump();
+    //         }
+    //     }
+    //
+    //     let mut w = Watcher(controller, HashMap::default());
+    //     loop {
+    //         thread::sleep(Duration::from_secs(10));
+    //         w.dump();
+    //     }
+    // });
 
     Ok(CollectorHandle {
         thread_handle: handle,
+        _controller: controller,
         producer,
     })
 }
