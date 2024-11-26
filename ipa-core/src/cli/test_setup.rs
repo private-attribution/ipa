d8fb347a7cb099edb31e0d6f593ffd1083bb2f14
Christian Berkhoff <berkhoff@meta.com>
2024-11-26 10:23:45 -0800

    temp

--- ipa-core/src/cli/test_setup.rs
+++ ipa-core/src/cli/test_setup.rs
@@ -34,10 +34,10 @@ pub struct TestSetupArgs {
     #[arg(long, default_value_t = false)]
     use_http1: bool,
 
-    #[arg(short, long, num_args = 3, value_name = "PORT", default_values = vec!["3000", "3001", "3002"])]
+    #[arg(short, long, value_name = "PORT", default_values = vec!["3000", "3001", "3002"])]
     ports: Vec<u16>,
 
-    #[arg(short, long, num_args = 3, value_name = "SHARD_PORT", default_values = vec!["6000", "6001", "6002"])]
+    #[arg(short, long, value_name = "SHARD_PORT", default_values = vec!["6000", "6001", "6002"])]
     shard_ports: Vec<u16>,
 }
 
@@ -49,6 +49,9 @@ pub struct TestSetupArgs {
 /// # Panics
 /// If something that shouldn't happen goes wrong.
 pub fn test_setup(args: TestSetupArgs) -> Result<(), BoxError> {
+    assert_eq!(args.ports.len(), args.shard_ports.len(), "number of mpc ports and shard ports don't match");
+    assert_eq!(args.ports.len() % 3, 0, "Number of ports must be a multiple of 3");
+
     if args.output_dir.exists() {
         if !args.output_dir.is_dir() || args.output_dir.read_dir()?.next().is_some() {
             return Err("output directory already exists and is not empty".into());
