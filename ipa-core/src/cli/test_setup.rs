92dbb2164f7124a0464f41d05c5ea28f3dd5bd8e
Christian Berkhoff <berkhoff@meta.com>
2024-11-26 16:29:34 -0800

    For Alex. Unit tests pass

--- ipa-core/src/cli/test_setup.rs
+++ ipa-core/src/cli/test_setup.rs
@@ -34,12 +34,12 @@ pub struct TestSetupArgs {
     #[arg(long, default_value_t = false)]
     use_http1: bool,
 
-    /// A list of ALL the MPC ports for all servers. If you have a server with shard count 4, you 
+    /// A list of ALL the MPC ports for all servers. If you have a server with shard count 4, you
     /// will have to provide 12 ports.
     #[arg(short, long, value_name = "PORT", default_values = vec!["3000", "3001", "3002"])]
     ports: Vec<u16>,
 
-    /// A list of ALL the sharding ports for all servers. If you have a server with shard count 4, 
+    /// A list of ALL the sharding ports for all servers. If you have a server with shard count 4,
     /// you will have to provide 12 ports.
     #[arg(short, long, value_name = "SHARD_PORT", default_values = vec!["6000", "6001", "6002"])]
     shard_ports: Vec<u16>,
@@ -53,9 +53,20 @@ pub struct TestSetupArgs {
 /// # Panics
 /// If something that shouldn't happen goes wrong.
 pub fn test_setup(args: TestSetupArgs) -> Result<(), BoxError> {
-    assert_eq!(args.ports.len(), args.shard_ports.len(), "number of mpc ports and shard ports don't match");
-    assert_eq!(args.ports.len() % 3, 0, "Number of ports must be a multiple of 3");
-    let shard_count = args.ports.len() % 3;
+    assert_eq!(
+        args.ports.len(),
+        args.shard_ports.len(),
+        "number of mpc ports and shard ports don't match"
+    );
+    assert_eq!(
+        args.ports.len() % 3,
+        0,
+        "Number of ports must be a multiple of 3"
+    );
+    assert!(
+        !args.ports.is_empty() && !args.shard_ports.is_empty(),
+        "Please provide a list of ports"
+    );
 
     if args.output_dir.exists() {
         if !args.output_dir.is_dir() || args.output_dir.read_dir()?.next().is_some() {
@@ -67,35 +78,128 @@ pub fn test_setup(args: TestSetupArgs) -> Result<(), BoxError> {
 
     let localhost = String::from("localhost");
 
-    // Create the leaders ring with MK keys
-    let clients_config: Vec<_> = zip([1, 2, 3], zip(args.ports, args.shard_ports))
-        .map(|(id, (port, shard_port))| {
-            let keygen_args = KeygenArgs {
-                name: localhost.clone(),
-                tls_cert: args.output_dir.helper_tls_cert(id),
-                tls_key: args.output_dir.helper_tls_key(id),
-                tls_expire_after: 365,
-                mk_public_key: Some(args.output_dir.helper_mk_public_key(id)),
-                mk_private_key: Some(args.output_dir.helper_mk_private_key(id)),
-            };
-
-            keygen(&keygen_args)?;
-
-            Ok(HelperClientConf {
-                host: localhost.to_string(),
-                port,
-                shard_port,
-                tls_cert_file: keygen_args.tls_cert,
-                mk_public_key_file: keygen_args.mk_public_key.unwrap(),
-            })
+    let keygen_args: Vec<_> = [1, 2, 3]
+        .into_iter()
+        .cycle()
+        .take(args.ports.len())
+        .enumerate()
+        .map(|(i, id)| {
+            //let helper_dir = .join(format!("helper{}", id));
+            //DirBuilder::new().create(&helper_dir).unwrap();
+            let shard_dir = args.output_dir.join(format!("shard{i}"));
+            DirBuilder::new().create(&shard_dir).unwrap();
+            if i < 3 {
+                // Only leader shards generate MK keys
+                KeygenArgs {
+                    name: localhost.clone(),
+                    tls_cert: shard_dir.helper_tls_cert(id),
+                    tls_key: shard_dir.helper_tls_key(id),
+                    tls_expire_after: 365,
+                    mk_public_key: Some(shard_dir.helper_mk_public_key(id)),
+                    mk_private_key: Some(shard_dir.helper_mk_private_key(id)),
+                }
+            } else {
+                KeygenArgs {
+                    name: localhost.clone(),
+                    tls_cert: shard_dir.helper_tls_cert(id),
+                    tls_key: shard_dir.helper_tls_key(id),
+                    tls_expire_after: 365,
+                    mk_public_key: None,
+                    mk_private_key: None,
+                }
+            }
         })
-        .collect::<Result<Vec<_>, BoxError>>()?
-        .try_into()
-        .unwrap();
+        .collect();
 
-    // Create the rest of the shards
+    for ka in &keygen_args {
+        keygen(ka)?;
+    }
 
+    let clients_config: Vec<_> = zip(
+        keygen_args.iter(),
+        zip(
+            keygen_args.clone().into_iter().take(3).cycle(),
+            zip(args.ports, args.shard_ports),
+        ),
+    )
+    .map(
+        |(keygen, (leader_keygen, (port, shard_port)))| HelperClientConf {
+            host: localhost.to_string(),
+            port,
+            shard_port,
+            tls_cert_file: keygen.tls_cert.clone(),
+            mk_public_key_file: leader_keygen.mk_public_key.clone().unwrap(),
+        },
+    )
+    .collect();
 
     let mut conf_file = File::create(args.output_dir.join("network.toml"))?;
     gen_client_config(clients_config.into_iter(), args.use_http1, &mut conf_file)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use tempfile::TempDir;
+
+    use crate::{
+        cli::{sharded_server_from_toml_str, test_setup, TestSetupArgs},
+        helpers::HelperIdentity,
+        sharding::ShardIndex,
+    };
+
+    #[test]
+    fn test_happy_case() {
+        let temp_dir = TempDir::new().unwrap();
+        let outdir = temp_dir.path().to_path_buf();
+        let args = TestSetupArgs {
+            output_dir: outdir.clone(),
+            disable_https: false,
+            use_http1: false,
+            ports: vec![3000, 3001, 3002, 3003, 3004, 3005],
+            shard_ports: vec![6000, 6001, 6002, 6003, 6004, 6005],
+        };
+        test_setup(args).unwrap();
+        let network_config_path = outdir.join("network.toml");
+        let network_config_string = &fs::read_to_string(network_config_path).unwrap();
+        let _r = sharded_server_from_toml_str(
+            network_config_string,
+            HelperIdentity::TWO,
+            ShardIndex::from(1),
+            ShardIndex::from(2),
+            None,
+        )
+        .unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "Please provide a list of ports")]
+    fn test_empty_ports() {
+        let temp_dir = TempDir::new().unwrap();
+        let outdir = temp_dir.path().to_path_buf();
+        let args = TestSetupArgs {
+            output_dir: outdir,
+            disable_https: false,
+            use_http1: false,
+            ports: vec![],
+            shard_ports: vec![],
+        };
+        test_setup(args).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "number of mpc ports and shard ports don't match")]
+    fn test_mismatched_ports() {
+        let temp_dir = TempDir::new().unwrap();
+        let outdir = temp_dir.path().to_path_buf();
+        let args = TestSetupArgs {
+            output_dir: outdir,
+            disable_https: false,
+            use_http1: false,
+            ports: vec![3000, 3001],
+            shard_ports: vec![6000, 6001, 6002],
+        };
+        test_setup(args).unwrap();
+    }
 }
