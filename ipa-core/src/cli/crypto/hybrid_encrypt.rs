25bdbb720ac79166b1edc1caa27a52f0bc7b33ed
Thomas James Yurek <tyurek@meta.com>
2024-12-04 17:19:25 -0800

    initial working run

---
+++ ipa-core/src/cli/crypto/hybrid_encrypt.rs
@@ -0,0 +1,228 @@
+use std::{
+    fs::{read_to_string, OpenOptions},
+    io::Write,
+    iter::zip,
+    path::{Path, PathBuf},
+};
+
+use clap::Parser;
+use rand::thread_rng;
+
+use crate::{
+    cli::{
+        config_parse::HelperNetworkConfigParseExt,
+        playbook::{BreakdownKey, InputSource, TriggerValue},
+    },
+    config::{KeyRegistries, NetworkConfig},
+    error::BoxError,
+    report::hybrid::{HybridReport, DEFAULT_KEY_ID},
+    secret_sharing::IntoShares,
+    test_fixture::hybrid::TestHybridRecord,
+};
+
+#[derive(Debug, Parser)]
+#[clap(name = "test_hybrid_encrypt", about = "Test Hybrid Encrypt")]
+#[command(about)]
+pub struct HybridEncryptArgs {
+    /// Path to file to secret share and encrypt
+    #[arg(long)]
+    input_file: PathBuf,
+    /// The destination dir for encrypted output.
+    /// In that dir, it will create helper1.enc,
+    /// helper2.enc, and helper3.enc
+    #[arg(long, value_name = "FILE")]
+    output_dir: PathBuf,
+    /// Path to helper network configuration file
+    #[arg(long)]
+    network: PathBuf,
+}
+
+impl HybridEncryptArgs {
+    #[must_use]
+    pub fn new(input_file: &Path, output_dir: &Path, network: &Path) -> Self {
+        Self {
+            input_file: input_file.to_path_buf(),
+            output_dir: output_dir.to_path_buf(),
+            network: network.to_path_buf(),
+        }
+    }
+
+    /// # Panics
+    /// if input file or network file are not correctly formatted
+    /// # Errors
+    /// if it cannot open the files
+    pub fn encrypt(&self) -> Result<(), BoxError> {
+        let input = InputSource::from_file(&self.input_file);
+
+        let mut rng = thread_rng();
+        let mut key_registries = KeyRegistries::default();
+
+        let network =
+            NetworkConfig::from_toml_str(&read_to_string(&self.network).unwrap_or_else(|e| {
+                panic!("Failed to open network file: {:?}. {}", &self.network, e)
+            }))
+            .unwrap_or_else(|e| {
+                panic!(
+                    "Failed to parse network file into toml: {:?}. {}",
+                    &self.network, e
+                )
+            });
+        let Some(key_registries) = key_registries.init_from(&network) else {
+            panic!("could not load network file")
+        };
+
+        let shares: [Vec<HybridReport<BreakdownKey, TriggerValue>>; 3] =
+            input.iter::<TestHybridRecord>().share();
+
+        for (index, (shares, key_registry)) in zip(shares, key_registries).enumerate() {
+            let output_filename = format!("helper{}.enc", index + 1);
+            let mut writer = OpenOptions::new()
+                .write(true)
+                .create_new(true)
+                .open(self.output_dir.join(&output_filename))
+                .unwrap_or_else(|e| panic!("unable write to {}. {}", &output_filename, e));
+
+            for share in shares {
+                let output = share
+                    .encrypt(DEFAULT_KEY_ID, key_registry, &mut rng)
+                    .unwrap();
+                let hex_output = hex::encode(&output);
+                writeln!(writer, "{hex_output}")?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(all(test, unit_test))]
+mod tests {
+    use std::io::Write;
+
+    use tempfile::{tempdir, NamedTempFile};
+
+    use crate::{
+        cli::{
+            crypto::{hybrid_encrypt::HybridEncryptArgs, sample_data},
+            CsvSerializer,
+        },
+        test_fixture::hybrid::TestHybridRecord,
+    };
+
+    #[tokio::test]
+    async fn try_encrypting_something() {
+        let helper_origin = "HELPER_ORIGIN".to_string();
+        let conversion_site_domain = "meta.com".to_string();
+        let records = vec![
+            TestHybridRecord::TestConversion {
+                match_key: 12345,
+                value: 2,
+                key_id: 0,
+                helper_origin: helper_origin.clone(),
+                conversion_site_domain: conversion_site_domain.clone(),
+                timestamp: 100,
+                epsilon: 0.0,
+                sensitivity: 0.0,
+            },
+            TestHybridRecord::TestConversion {
+                match_key: 12345,
+                value: 5,
+                key_id: 0,
+                helper_origin: helper_origin.clone(),
+                conversion_site_domain: conversion_site_domain.clone(),
+                timestamp: 101,
+                epsilon: 0.0,
+                sensitivity: 0.0,
+            },
+            TestHybridRecord::TestImpression {
+                match_key: 23456,
+                breakdown_key: 4,
+                key_id: 0,
+                helper_origin: helper_origin.clone(),
+            },
+        ];
+        let mut input_file = NamedTempFile::new().unwrap();
+
+        for event in records {
+            event.to_csv(input_file.as_file_mut()).unwrap();
+            writeln!(input_file.as_file()).unwrap();
+        }
+        input_file.flush().unwrap();
+
+        let output_dir = tempdir().unwrap();
+        let network_file = sample_data::test_keys().network_config();
+
+        HybridEncryptArgs::new(input_file.path(), output_dir.path(), network_file.path())
+            .encrypt()
+            .unwrap();
+
+        /*let files = [
+            &output_dir.path().join("helper1.enc"),
+            &output_dir.path().join("helper2.enc"),
+            &output_dir.path().join("helper3.enc"),
+        ];
+
+        let world = TestWorld::default();
+
+        let mk_private_keys = [
+            "53d58e022981f2edbf55fec1b45dbabd08a3442cb7b7c598839de5d7a5888bff",
+            "3a0a993a3cfc7e8d381addac586f37de50c2a14b1a6356d71e94ca2afaeb2569",
+            "1fb5c5274bf85fbe6c7935684ef05499f6cfb89ac21640c28330135cc0e8a0f7",
+        ];*/
+    }
+
+    #[test]
+    #[should_panic = "Failed to open network file:"]
+    fn encrypt_no_network_file() {
+        let input_file = sample_data::write_csv(sample_data::test_ipa_data().take(10)).unwrap();
+
+        let output_dir = tempdir().unwrap();
+        let network_dir = tempdir().unwrap();
+        let network_file = network_dir.path().join("does_not_exist");
+        HybridEncryptArgs::new(input_file.path(), output_dir.path(), &network_file)
+            .encrypt()
+            .unwrap();
+    }
+
+    #[test]
+    #[should_panic = "TOML parse error at"]
+    fn encrypt_bad_network_file() {
+        let input_file = sample_data::write_csv(sample_data::test_ipa_data().take(10)).unwrap();
+        let output_dir = tempdir().unwrap();
+        let network_data = r"
+this is not toml!
+%^& weird characters
+(\deadbeef>?
+";
+        let mut network_file = NamedTempFile::new().unwrap();
+        writeln!(network_file.as_file_mut(), "{network_data}").unwrap();
+
+        HybridEncryptArgs::new(input_file.path(), output_dir.path(), network_file.path())
+            .encrypt()
+            .unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "Failed to parse network file into toml")]
+    fn encrypt_incomplete_network_file() {
+        let input_file = sample_data::write_csv(sample_data::test_ipa_data().take(10)).unwrap();
+
+        let output_dir = tempdir().unwrap();
+        let network_data = r#"
+[[peers]]
+url = "helper1.test"
+[peers.hpke]
+public_key = "92a6fb666c37c008defd74abf3204ebea685742eab8347b08e2f7c759893947a"
+[[peers]]
+url = "helper2.test"
+[peers.hpke]
+public_key = "cfdbaaff16b30aa8a4ab07eaad2cdd80458208a1317aefbb807e46dce596617e"
+"#;
+        let mut network_file = NamedTempFile::new().unwrap();
+        writeln!(network_file.as_file_mut(), "{network_data}").unwrap();
+
+        HybridEncryptArgs::new(input_file.path(), output_dir.path(), network_file.path())
+            .encrypt()
+            .unwrap();
+    }
+}
--- ipa-core/src/cli/crypto/mod.rs
+++ ipa-core/src/cli/crypto/mod.rs
@@ -1,8 +1,12 @@
 mod decrypt;
 mod encrypt;
+mod hybrid_decrypt;
+mod hybrid_encrypt;
 
 pub use decrypt::DecryptArgs;
 pub use encrypt::EncryptArgs;
+pub use hybrid_decrypt::HybridDecryptArgs;
+pub use hybrid_encrypt::HybridEncryptArgs;
 
 #[cfg(test)]
 mod sample_data {
@@ -137,6 +141,142 @@ mod sample_data {
     }
 }
 
+#[cfg(test)]
+mod hybrid_sample_data {
+    use std::{io, io::Write, sync::OnceLock};
+
+    use hpke::{Deserializable, Serializable};
+    use rand::thread_rng;
+    use tempfile::NamedTempFile;
+
+    use crate::{
+        cli::CsvSerializer,
+        hpke::{IpaPrivateKey, IpaPublicKey},
+        test_fixture::{
+            hybrid::TestHybridRecord, hybrid_event_gen::ConversionDistribution,
+            HybridEventGenerator, HybridGeneratorConfig,
+        },
+    };
+
+    /// Keys that are used in crypto tests
+    #[derive(Clone)]
+    pub(super) struct TestKeys {
+        key_pairs: [(IpaPublicKey, IpaPrivateKey); 3],
+    }
+
+    static TEST_KEYS: OnceLock<TestKeys> = OnceLock::new();
+    pub fn test_keys() -> &'static TestKeys {
+        TEST_KEYS.get_or_init(TestKeys::new)
+    }
+
+    impl TestKeys {
+        pub fn new() -> Self {
+            Self {
+                key_pairs: [
+                    (
+                        decode_key::<_, IpaPublicKey>(
+                            "92a6fb666c37c008defd74abf3204ebea685742eab8347b08e2f7c759893947a",
+                        ),
+                        decode_key::<_, IpaPrivateKey>(
+                            "53d58e022981f2edbf55fec1b45dbabd08a3442cb7b7c598839de5d7a5888bff",
+                        ),
+                    ),
+                    (
+                        decode_key::<_, IpaPublicKey>(
+                            "cfdbaaff16b30aa8a4ab07eaad2cdd80458208a1317aefbb807e46dce596617e",
+                        ),
+                        decode_key::<_, IpaPrivateKey>(
+                            "3a0a993a3cfc7e8d381addac586f37de50c2a14b1a6356d71e94ca2afaeb2569",
+                        ),
+                    ),
+                    (
+                        decode_key::<_, IpaPublicKey>(
+                            "b900be35da06106a83ed73c33f733e03e4ea5888b7ea4c912ab270b0b0f8381e",
+                        ),
+                        decode_key::<_, IpaPrivateKey>(
+                            "1fb5c5274bf85fbe6c7935684ef05499f6cfb89ac21640c28330135cc0e8a0f7",
+                        ),
+                    ),
+                ],
+            }
+        }
+
+        pub fn network_config(&self) -> NamedTempFile {
+            let mut file = NamedTempFile::new().unwrap();
+            let [pk1, pk2, pk3] = self.key_pairs.each_ref().map(|(pk, _)| pk);
+            let [pk1, pk2, pk3] = [
+                hex::encode(pk1.to_bytes()),
+                hex::encode(pk2.to_bytes()),
+                hex::encode(pk3.to_bytes()),
+            ];
+            let network_data = format!(
+                r#"
+                [[peers]]
+                url = "helper1.test"
+                [peers.hpke]
+                public_key = "{pk1}"
+                [[peers]]
+                url = "helper2.test"
+                [peers.hpke]
+                public_key = "{pk2}"
+                [[peers]]
+                url = "helper3.test"
+                [peers.hpke]
+                public_key = "{pk3}"
+            "#
+            );
+            file.write_all(network_data.as_bytes()).unwrap();
+
+            file
+        }
+
+        pub fn set_sk<I: AsRef<[u8]>>(&mut self, idx: usize, data: I) {
+            self.key_pairs[idx].1 = IpaPrivateKey::from_bytes(data.as_ref()).unwrap();
+        }
+
+        pub fn get_sk(&self, idx: usize) -> Vec<u8> {
+            self.key_pairs[idx].1.to_bytes().to_vec()
+        }
+
+        pub fn sk_files(&self) -> [NamedTempFile; 3] {
+            self.key_pairs.each_ref().map(|(_, sk)| sk).map(|sk| {
+                let mut file = NamedTempFile::new().unwrap();
+                file.write_all(hex::encode(sk.to_bytes()).as_bytes())
+                    .unwrap();
+                file.flush().unwrap();
+
+                file
+            })
+        }
+    }
+
+    fn decode_key<I: AsRef<[u8]>, T: Deserializable>(input: I) -> T {
+        let bytes = hex::decode(input).unwrap();
+        T::from_bytes(&bytes).unwrap()
+    }
+
+    pub fn test_hybrid_data() -> impl Iterator<Item = TestHybridRecord> {
+        let rng = thread_rng();
+        let event_gen_args = HybridGeneratorConfig::new(3, 50, ConversionDistribution::Default);
+
+        HybridEventGenerator::with_config(rng, event_gen_args)
+    }
+
+    pub fn write_csv<C: CsvSerializer>(
+        data: impl Iterator<Item = C>,
+    ) -> Result<NamedTempFile, io::Error> {
+        let mut file = NamedTempFile::new()?;
+        for event in data {
+            let () = event.to_csv(&mut file)?;
+            writeln!(file)?;
+        }
+
+        file.flush()?;
+
+        Ok(file)
+    }
+}
+
 #[cfg(all(test, unit_test))]
 mod tests {
     use std::{
@@ -147,7 +287,10 @@ mod tests {
 
     use tempfile::tempdir;
 
-    use crate::cli::crypto::{decrypt::DecryptArgs, encrypt::EncryptArgs, sample_data};
+    use crate::cli::crypto::{
+        decrypt::DecryptArgs, encrypt::EncryptArgs, hybrid_decrypt::HybridDecryptArgs,
+        hybrid_encrypt::HybridEncryptArgs, hybrid_sample_data, sample_data,
+    };
 
     fn are_files_equal(file1: &Path, file2: &Path) {
         let file1 =
@@ -181,6 +324,39 @@ mod tests {
             sample_data::test_keys().sk_files();
 
         DecryptArgs::new(
+            enc1.as_path(),
+            enc2.as_path(),
+            enc3.as_path(),
+            mk_private_key1.path(),
+            mk_private_key2.path(),
+            mk_private_key3.path(),
+            &decrypt_output,
+        )
+        .decrypt_and_reconstruct()
+        .await
+        .unwrap();
+
+        are_files_equal(input_file.path(), &decrypt_output);
+    }
+
+    #[tokio::test]
+    async fn hybrid_encrypt_and_decrypt() {
+        let output_dir = tempdir().unwrap();
+        let input = hybrid_sample_data::test_hybrid_data().take(10);
+        let input_file = hybrid_sample_data::write_csv(input).unwrap();
+        let network_file = hybrid_sample_data::test_keys().network_config();
+        HybridEncryptArgs::new(input_file.path(), output_dir.path(), network_file.path())
+            .encrypt()
+            .unwrap();
+
+        let decrypt_output = output_dir.path().join("output");
+        let enc1 = output_dir.path().join("helper1.enc");
+        let enc2 = output_dir.path().join("helper2.enc");
+        let enc3 = output_dir.path().join("helper3.enc");
+        let [mk_private_key1, mk_private_key2, mk_private_key3] =
+            hybrid_sample_data::test_keys().sk_files();
+
+        HybridDecryptArgs::new(
             enc1.as_path(),
             enc2.as_path(),
             enc3.as_path(),
