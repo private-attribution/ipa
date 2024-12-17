0cad76d486f906bec743256a8b22aff2b0ef343e
Erik Taubeneck <erik.taubeneck@gmail.com>
2024-12-17 15:30:55 -0800

    updates

--- scripts/split-encrypted-files.py
+++ scripts/split-encrypted-files.py
@@ -1,8 +1,16 @@
 import argparse
 import binascii
 
+try:
+    from tqdm import tqdm
+except ImportError:
+    print("tqdm not installed. run `pip install tqdm` to see progress")
 
-def split_hex_file(input_filename, num_files):
+    def tqdm(iterable, *args, **kwargs):
+        return iterable
+
+
+def split_hex_file(input_filename, output_stem, num_files):
     """
     Reads in a file of hex strings, one per line, splits it up into N files,
     and writes out each line as length-delimited binary data.
@@ -10,10 +18,12 @@ def split_hex_file(input_filename, num_files):
     :param input_filename: The name of the input file containing hex strings.
     :param num_files: The number of output files to split the input into.
     """
-    output_files = [open(f"{input_filename}_{i}.bin", "wb") for i in range(num_files)]
+    output_files = [
+        open(f"{output_stem}_shard_{i}.bin", "wb") for i in range(num_files)
+    ]
 
     with open(input_filename, "r") as input_file:
-        for i, line in enumerate(input_file):
+        for i, line in enumerate(tqdm(input_file, desc="Processing lines")):
             # Remove any leading or trailing whitespace from the line
             line = line.strip()
 
@@ -35,7 +45,13 @@ if __name__ == "__main__":
         description="Splits a file of hex strings into N length-delimited binary files"
     )
     parser.add_argument(
-        "-i", "--input", required=True, help="Input file containing hex strings"
+        "-i", "--input_file", required=True, help="Input file containing hex strings"
+    )
+    parser.add_argument(
+        "-o",
+        "--output_stem",
+        required=True,
+        help="Output file stem for generated files",
     )
     parser.add_argument(
         "-n",
@@ -46,4 +62,4 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    split_hex_file(args.input, args.num_files)
+    split_hex_file(args.input_file, args.output_stem, args.num_files)
